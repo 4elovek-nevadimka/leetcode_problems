1829a0576b467ee90ad099dd1e9bbe57fbd0bb86
nevadimka <morozovvadimka@gmail.com>
2023-10-19 21:56:35 +0300

    Added LeetCode Problem #0844

---
+++ LeetCodeProblems/Solutions/Task_0844. Backspace String Compare.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace Solutions
+{
+    public class Task_0844
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
+            return Solution1(s, t);
+        }
+
+        private bool Solution1(string s, string t)
+        {
+            return GetFormattedString(s) == GetFormattedString(t);
+        }
+
+        private string GetFormattedString(string str)
+        {
+            var index = str.Length - 1;
+            var skipChars = 0;
+            var strBuilder = new StringBuilder();
+            while (index >= 0)
+            {
+                if (str[index] == '#')
+                {
+                    skipChars++;
+                }
+                else
+                {
+                    if (skipChars > 0)
+                    {
+                        skipChars--;
+                    }
+                    else
+                    {
+                        strBuilder.Append(str[index]);
+                    }
+                }
+                index--;
+            }
+            return strBuilder.ToString();
+        }
+
+        public void Run()
+        {
+            var s = "ab#c";
+            var t = "ad#c";
+
+            var result = BackspaceCompare(s, t);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ LeetCodeProblems/Tests/Test_0844. Backspace String Compare.cs
@@ -0,0 +1,70 @@
+﻿using Solutions;
+
+namespace Tests
+{
+    internal class Test_0844
+    {
+        private Task_0844 _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new Task_0844();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var s = "ab#c";
+            var t = "ad#c";
+            var expected = true;
+
+            var result = _solution.BackspaceCompare(s, t);
+            Assert.That(result, Is.EqualTo(expected), $"Wrong Answer!");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var s = "ab##";
+            var t = "c#d#";
+            var expected = true;
+
+            var result = _solution.BackspaceCompare(s, t);
+            Assert.That(result, Is.EqualTo(expected), $"Wrong Answer!");
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var s = "a#c";
+            var t = "b";
+            var expected = false;
+
+            var result = _solution.BackspaceCompare(s, t);
+            Assert.That(result, Is.EqualTo(expected), $"Wrong Answer!");
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var s = "a##c";
+            var t = "#a#c";
+            var expected = true;
+
+            var result = _solution.BackspaceCompare(s, t);
+            Assert.That(result, Is.EqualTo(expected), $"Wrong Answer!");
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var s = "y#fo##f";
+            var t = "y#f#o##f";
+            var expected = true;
+
+            var result = _solution.BackspaceCompare(s, t);
+            Assert.That(result, Is.EqualTo(expected), $"Wrong Answer!");
+        }
+    }
+}
