052babac834c50ba1c0105b316fbb0193d85c8eb
nevadimka <morozovvadimka@gmail.com>
2023-10-24 20:31:53 +0300

    Added LeetCode Problem #0515

---
+++ LeetCodeProblems/Solutions/Task_0515. Find Largest Value in Each Tree Row.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Task_0515
+    {
+        public IList<int> LargestValues(TreeNode root)
+        {
+            return Solution2(root);
+        }
+
+        private IList<int> Solution2(TreeNode root)
+        {
+            if (root == null)
+                return new List<int>();
+
+            var largestValues = new List<int>();
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var currentLevelMax = int.MinValue;
+                var nodesOnLevel = queue.Count;
+
+                for (var i = 0; i < nodesOnLevel; i++)
+                {
+                    var node = queue.Dequeue();
+                    currentLevelMax = Math.Max(currentLevelMax, node.val);
+
+                    if (node.left != null)
+                        queue.Enqueue(node.left);
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+
+                }
+                largestValues.Add(currentLevelMax);
+            }
+            return largestValues;
+        }
+
+        private IList<int> Solution1(TreeNode root)
+        {
+            if (root == null)
+                return new List<int>();
+
+            var largestValues = new List<int> { root.val };
+
+            var nextLevelNodes = new Queue<TreeNode>();
+            if (root.left != null)
+                nextLevelNodes.Enqueue(root.left);
+            if (root.right != null)
+                nextLevelNodes.Enqueue(root.right);
+
+            if (nextLevelNodes.Count > 0)
+                BFS(largestValues, nextLevelNodes);
+
+            return largestValues;
+        }
+
+        private void BFS(IList<int> largestValues, Queue<TreeNode> currentLevelNodes)
+        {
+            var currentLevelMax = int.MinValue;
+            var nextLevelNodes = new Queue<TreeNode>();
+
+            while (currentLevelNodes.Count > 0)
+            {
+                var node = currentLevelNodes.Dequeue();
+                if (node.val > currentLevelMax)
+                    currentLevelMax = node.val;
+
+                if (node.left != null)
+                    nextLevelNodes.Enqueue(node.left);
+                if (node.right != null)
+                    nextLevelNodes.Enqueue(node.right);
+            }
+
+            largestValues.Add(currentLevelMax);
+
+            if (nextLevelNodes.Count > 0)
+                BFS(largestValues, nextLevelNodes);
+        }
+
+        public void Run()
+        {
+
+        }
+    }
+}
