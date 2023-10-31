068874b048b9761abfef9feb2e4755772ccba10b
nevadimka <morozovvadimka@gmail.com>
2023-10-31 20:24:12 +0300

    Added LeetCode Problem #1356

---
+++ LeetCodeProblems/Solutions/Task_1356. Sort Integers by The Number of 1 Bits.cs
@@ -0,0 +1,92 @@
+﻿namespace Solutions
+{
+    public class Task_1356
+    {
+        public void Run()
+        {
+            // var arr = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            var arr = new[] { 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 };
+            var result = SortByBits(arr);
+            Array.ForEach(result, Console.WriteLine);
+        }
+
+        public int[] SortByBits(int[] arr)
+        {
+            return Solution3(arr);
+        }
+
+        private int[] Solution3(int[] arr)
+        {
+            Array.Sort(arr, (a, b) => Compare(a, b));
+            return arr;
+        }
+
+        private int Compare(int left, int right)
+        {
+            var res = CountSetBits(left) - CountSetBits(right);
+            if (res != 0) return res;
+
+            return left - right;
+        }
+
+        private int[] Solution2(int[] arr)
+        {
+            for (var i = 0; i < arr.Length; i++)
+            {
+                int smallestBinValue = CountSetBits(arr[i]);
+                int smallestValue = arr[i], smallestIndex = -1;
+                for (var j = i + 1; j < arr.Length; j++)
+                {
+                    var currentBinValue = CountSetBits(arr[j]);
+                    if (currentBinValue < smallestBinValue)
+                    {
+                        smallestBinValue = currentBinValue;
+                        smallestValue = arr[j];
+                        smallestIndex = j;
+                    }
+                    else if (currentBinValue == smallestBinValue)
+                    {
+                        if (arr[j] < smallestValue)
+                        {
+                            smallestValue = arr[j];
+                            smallestIndex = j;
+                        }
+                    }
+                }
+                if (smallestIndex > -1)
+                {
+                    (arr[i], arr[smallestIndex]) = (arr[smallestIndex], arr[i]);
+                }
+            }
+            return arr;
+        }
+
+
+        private int[] Solution1(int[] arr)
+        {
+            var pairs = new List<(int dec, int bin)>();
+            foreach (var n in arr)
+            {
+                pairs.Add((n, CountSetBits(n)));
+            }
+            return pairs.OrderBy(x => x.bin).ThenBy(x => x.dec).Select(x => x.dec).ToArray();
+        }
+
+        private int CountSetBits(int n)
+        {
+            var setBits = 0;
+            while (n > 0)
+            {
+                if ((n & 1) == 1)
+                    setBits++;
+                n >>= 1;
+            }
+            return setBits;
+        }
+
+        private int CountSetBits2(int n)
+        {
+            return Convert.ToString(n, 2).Count(x => x == '1');
+        }
+    }
+}
---
+++ LeetCodeProblems/Tests/Test_1356. Sort Integers by The Number of 1 Bits.cs
@@ -0,0 +1,45 @@
+﻿using Solutions;
+
+namespace Tests
+{
+    internal class Test_1356
+    {
+        private Task_1356 _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new Task_1356();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var arr = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            var expected = new[] { 0, 1, 2, 4, 8, 3, 5, 6, 7 };
+
+            var result = _solution.SortByBits(arr);
+            Assert.That(result, Is.EqualTo(expected), $"Wrong Answer!");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var arr = new[] { 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 };
+            var expected = new[] { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 };
+
+            var result = _solution.SortByBits(arr);
+            Assert.That(result, Is.EqualTo(expected), $"Wrong Answer!");
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var arr = new[] { 7850, 8192, 1878, 2687, 6878, 7410, 763, 365, 9869, 8744, 2741, 4871, 5844, 2715, 3335, 9651, 6677 };
+            var expected = new[] { 8192, 8744, 365, 3335, 4871, 6677, 1878, 2715, 2741, 5844, 9869, 763, 7410, 7850, 9651, 2687, 6878 };
+
+            var result = _solution.SortByBits(arr);
+            Assert.That(result, Is.EqualTo(expected), $"Wrong Answer!");
+        }
+    }
+}
