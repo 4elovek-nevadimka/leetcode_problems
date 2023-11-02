a847ea7667235b846589543f63bccbd46e044444
nevadimka <morozovvadimka@gmail.com>
2023-11-02 22:11:47 +0300

    Added LeetCode Problem #2265

---
+++ LeetCodeProblems/Solutions/Task_2265. Count Nodes Equal to Average of Subtree.cs
@@ -0,0 +1,28 @@
+﻿namespace Solutions
+{
+    internal class Task_2265
+    {
+        private int avgCounter = 0;
+
+        public int AverageOfSubtree(TreeNode root) {
+            Traverse(root);
+            return avgCounter;
+        }
+
+        private (int nodes, int sum) Traverse(TreeNode node)
+        {
+            if (node == null) return (0, 0);
+
+            var left = Traverse(node.left);
+
+            var right = Traverse(node.right);
+
+            var nodes = left.nodes + right.nodes + 1;
+            var sum = left.sum + right.sum + node.val;
+
+            if (sum / nodes == node.val) avgCounter++;
+            
+            return (nodes, sum);
+        }
+    }
+}
