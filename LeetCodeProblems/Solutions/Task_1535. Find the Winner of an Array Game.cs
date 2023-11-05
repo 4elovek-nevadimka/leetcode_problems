9b180590101e4556f9b4c04853779e7cc4f86384
nevadimka <morozovvadimka@gmail.com>
2023-11-05 18:52:44 +0300

    Added LeetCode Problem #1535

---
+++ LeetCodeProblems/Solutions/Task_1535. Find the Winner of an Array Game.cs
@@ -0,0 +1,24 @@
+﻿namespace Solutions
+{
+    internal class Task_1535
+    {
+        public int GetWinner(int[] arr, int k)
+        {
+            int max = arr[0], currentK = 0;
+            for (var i = 1; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                    currentK = 1;
+                }
+                else
+                {
+                    currentK++;
+                }
+                if (currentK == k) break;
+            }
+            return max;
+        }
+    }
+}
