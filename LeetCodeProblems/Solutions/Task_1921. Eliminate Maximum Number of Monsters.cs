b406996707260e7ee19a63f07aafb05a2937861e
nevadimka <morozovvadimka@gmail.com>
2023-11-07 22:16:16 +0300

    Added LeetCode Problem #1921

---
+++ LeetCodeProblems/Solutions/Task_1921. Eliminate Maximum Number of Monsters.cs
@@ -0,0 +1,27 @@
+﻿namespace Solutions
+{
+    internal class Task_1921
+    {
+        public int EliminateMaximum(int[] dist, int[] speed)
+        {
+            var n = dist.Length;
+            var steps = new double[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                // steps to finish
+                steps[i] = (double)dist[i] / speed[i];
+            }
+            Array.Sort(steps);
+
+            for (var i = 0; i < n; i++)
+            {
+                if (steps[i] <= i)
+                {
+                    return i;
+                }
+            }
+            return n;
+        }
+    }
+}
