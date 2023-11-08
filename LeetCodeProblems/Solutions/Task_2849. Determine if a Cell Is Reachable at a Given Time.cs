c160b7a0ff1c1960c05a7c4e050a7dcf3975d462
nevadimka <morozovvadimka@gmail.com>
2023-11-08 16:41:15 +0300

    Added LeetCode Problem #2849

---
+++ LeetCodeProblems/Solutions/Task_2849. Determine if a Cell Is Reachable at a Given Time.cs
@@ -0,0 +1,14 @@
+﻿namespace Solutions
+{
+    internal class Task_2849
+    {
+        public bool IsReachableAtTime(int sx, int sy, int fx, int fy, int t)
+        {
+            if (t == 1 && sx == fx && sy == fy)
+                return false;
+
+            var minDistance = Math.Max(Math.Abs(fx - sx), Math.Abs(fy - sy));
+            return minDistance <= t;
+        }
+    }
+}
