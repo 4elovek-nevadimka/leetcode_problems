921483f1f3749868c19b3303253339822a7d1822
nevadimka <morozovvadimka@gmail.com>
2023-11-09 16:50:30 +0300

    Added LeetCode Problem #1759

---
+++ LeetCodeProblems/Solutions/Task_1759. Count Number of Homogenous Substrings.cs
@@ -0,0 +1,54 @@
+﻿namespace Solutions
+{
+    internal class Task_1759
+    {
+        public int CountHomogenous(string s)
+        {
+            const int MOD = 1000000007;
+
+            int charCounter = 1, homogenousCount = 0;
+            for (var i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    charCounter++;
+                }
+                else
+                {
+                    homogenousCount +=
+                        (int)((long)charCounter * (charCounter + 1) / 2 % MOD);
+                    charCounter = 1;
+                }
+            }
+            homogenousCount +=
+                (int)((long)charCounter * (charCounter + 1) / 2 % MOD);
+            return homogenousCount;
+        }
+
+        public int CountHomogenous2(string s)
+        {
+            // modulo 10^9 + 7
+            const int MOD = 1000000007;
+
+            int charCounter = 0, homogenousCount = 0;
+
+            char previousChar = s[0];
+            foreach (var c in s)
+            {
+                if (c == previousChar)
+                {
+                    charCounter++;
+                }
+                else
+                {
+                    // arithmetic progression
+                    homogenousCount += (int)((long)charCounter * (charCounter + 1) / 2 % MOD);
+                    previousChar = c;
+                    charCounter = 1;
+                }
+            }
+            homogenousCount += (int)((long)charCounter * (charCounter + 1) / 2 % MOD);
+            return homogenousCount;
+        }
+    }
+}
