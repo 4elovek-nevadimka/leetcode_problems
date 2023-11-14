58990b1ce36f9379f434ecc98bde81cecd0220b2
nevadimka <morozovvadimka@gmail.com>
2023-11-14 21:47:18 +0300

    Added LeetCode Problem #1930

---
+++ LeetCodeProblems/Solutions/Task_1930. Unique Length-3 Palindromic Subsequences.cs
@@ -0,0 +1,51 @@
+﻿namespace Solutions
+{
+    internal class Task_1930
+    {
+        public void Run()
+        {
+            // var s = "aabca";
+            var s = "bbcbaba";
+
+            Console.WriteLine(CountPalindromicSubsequence(s));
+        }
+
+        public int CountPalindromicSubsequence(string s)
+        {
+            var dic = new Dictionary<char, List<int>>();
+            var palindromicCounter = 0;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (!dic.ContainsKey(s[i]))
+                    dic.Add(s[i], new List<int>());
+                dic[s[i]].Add(i);
+            }
+
+            foreach (var c in dic.Keys)
+            {
+                // skip
+                if (dic[c].Count == 1) continue;
+                // add xxx palindrom
+                if (dic[c].Count > 2) palindromicCounter++;
+                // check others
+                int minIndex = dic[c][0], maxIndex = dic[c][^1];
+                foreach (var other in dic.Keys)
+                {
+                    if (c == other) continue;
+
+                    foreach (var index in dic[other])
+                    {
+                        if (index > minIndex && index < maxIndex)
+                        {
+                            palindromicCounter++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return palindromicCounter;
+        }
+    }
+}
