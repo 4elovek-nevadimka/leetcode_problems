055a066e0b3f61ed43a43b59ab8aad51dd8c6f6a
nevadimka <morozovvadimka@gmail.com>
2023-11-18 21:59:29 +0300

    Added LeetCode Problem #1838

---
+++ LeetCodeProblems/Solutions/Task_1838. Frequency of the Most Frequent Element.cs
@@ -0,0 +1,39 @@
+﻿namespace Solutions
+{
+    internal class Task_1838
+    {
+        public void Run()
+        {
+            var nums = new[] { 1, 2, 4 };
+            // var nums = new[] { 1, 4, 8, 13 };
+            var k = 5;
+
+            Console.WriteLine(MaxFrequency(nums, k));
+        }
+
+        public int MaxFrequency(int[] nums, int k)
+        {
+            return Solution1(nums, k);
+        }
+
+        private int Solution1(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            
+            int left = 0, maxFrequency = 0;
+            long sum = 0;
+
+            for (var right = 0; right < nums.Length; right++)
+            {
+                sum += nums[right];
+                while ((long)(nums[right] * (right - left + 1)) - sum > k)
+                {
+                    sum -= nums[left];
+                    left++;
+                }
+                maxFrequency = Math.Max(maxFrequency, right - left + 1);
+            }
+            return maxFrequency;
+        }
+    }
+}
