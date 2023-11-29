486417c0d9848c3cd58ea1ea979a7dd6383a004d
nevadimka <morozovvadimka@gmail.com>
2023-11-29 20:04:17 +0300

    Added LeetCode Problem #0191

---
+++ LeetCodeProblems/Solutions/Task_0191. Number of 1 Bits.cs
@@ -0,0 +1,42 @@
+﻿namespace Solutions
+{
+    internal class Task_0191
+    {
+        public void Run()
+        {
+            uint n = 00000000000000000000000000001011;
+            Console.WriteLine(HammingWeight(n));
+        }
+
+        public int HammingWeight(uint n)
+        {
+            return Solution2(n);
+        }
+
+        private int Solution1(uint n)
+        {
+            int setBits = 0;
+            while (n > 0)
+            {
+                if ((n & 1) == 1)
+                {
+                    setBits++;
+                }
+                n = n >> 1;
+            }
+            return setBits;
+        }
+
+        private int Solution2(uint n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                count++;
+                // clear the least significant set bit
+                n &= (n - 1);
+            }
+            return count;
+        }
+    }
+}
