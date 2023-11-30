caf2af86cc03ca502eb4cbcc57179cac81d051d5
nevadimka <morozovvadimka@gmail.com>
2023-11-30 21:25:26 +0300

    Added LeetCode Problem #1611

---
+++ LeetCodeProblems/Solutions/Task_1611. Minimum One Bit Operations to Make Integers Zero.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Solutions
+{
+    internal class Task_1611
+    {
+        public int MinimumOneBitOperations(int n)
+        {
+            if (n == 0) return 0;
+            return n ^ MinimumOneBitOperations(n >> 1);
+        }
+    }
+}
