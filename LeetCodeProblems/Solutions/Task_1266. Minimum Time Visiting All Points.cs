90d14ecf69d3f0a1ac4fcecbc84385f4fe832f6d
nevadimka <morozovvadimka@gmail.com>
2023-12-03 06:29:59 +0300

    Added LeetCode Problem #1266

---
+++ LeetCodeProblems/Solutions/Task_1266. Minimum Time Visiting All Points.cs
@@ -0,0 +1,18 @@
+﻿namespace Solutions
+{
+    internal class Task_1266
+    {
+        public int MinTimeToVisitAllPoints(int[][] points)
+        {
+            var minTime = 0;
+            for (var i = 1; i < points.Length; i++)
+            {
+                minTime += Math.Max(
+                    Math.Abs(points[i][0] - points[i - 1][0]),
+                    Math.Abs(points[i][1] - points[i - 1][1]));
+            }
+
+            return minTime;
+        }
+    }
+}
