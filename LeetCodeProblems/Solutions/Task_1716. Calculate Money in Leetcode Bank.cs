a0d52eedc618c5821e3b5fbd1e60c6d32b3d009a
nevadimka <morozovvadimka@gmail.com>
2023-12-06 16:53:44 +0300

    Added LeetCode Problem #1716

---
+++ LeetCodeProblems/Solutions/Task_1716. Calculate Money in Leetcode Bank.cs
@@ -0,0 +1,39 @@
+﻿namespace Solutions
+{
+    internal class Task_1716
+    {
+        public void Run()
+        {
+            Console.WriteLine(TotalMoney(20));
+        }
+
+        public int TotalMoney(int n)
+        {
+            if (n <= 7)
+            {
+                return n * (n + 1) / 2;
+            }
+            var currentWeek = 7 * (7 + 1) / 2;
+            var totalMoney = currentWeek;
+            var fulllWeeks = n / 7;
+            for (var i = 1; i < fulllWeeks; i++)
+            {
+                currentWeek += 7;
+                totalMoney += currentWeek;
+            }
+
+            var reminderOfWeek = n % 7;
+            if (reminderOfWeek > 0)
+            {
+                var currentReminder = 1 + fulllWeeks;
+                totalMoney += currentReminder;
+                for (var i = 1; i < reminderOfWeek; i++)
+                {
+                    totalMoney += ++currentReminder;
+                }
+            }
+
+            return totalMoney;
+        }
+    }
+}
