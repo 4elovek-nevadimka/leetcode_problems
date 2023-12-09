e948b755c61b8321162ac2c1276b337e36edf484
nevadimka <morozovvadimka@gmail.com>
2023-12-09 20:00:30 +0300

    Added LeetCode Problem #0094

--- LeetCodeProblems/Solutions/Program.cs
+++ LeetCodeProblems/Solutions/Program.cs
@@ -1,8 +1,9 @@
 ﻿using Solutions;
+using Solutions.Trees;
 
 Console.WriteLine("LeetCode Problems!");
 
 // run task here for debugging
-new Task_0606().Run();
+new Task_0094().Run();
 
 // Console.ReadKey();
---
+++ LeetCodeProblems/Solutions/Trees/Task_0094. Binary Tree Inorder Traversal.cs
@@ -0,0 +1,54 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_0094
+    {
+        public void Run()
+        {
+            var root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3)));
+            
+            var nodesList = InorderTraversal(root);
+            foreach (var node in nodesList)
+            {
+                Console.WriteLine(node);
+            }
+        }
+
+        // DFS Solution
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null) return new List<int>();
+            
+            var list = new List<int>();
+            list.AddRange(InorderTraversal(root.left));
+
+            list.Add(root.val);
+
+            list.AddRange(InorderTraversal(root.right));
+
+            return list;
+            
+        }
+
+        // Stack Solution
+        public IList<int> InorderTraversal2(TreeNode root)
+        {
+            var list = new List<int>();
+            var stack = new Stack<TreeNode>();
+            while (stack.Count != 0 || root != null)
+            {
+                if (root != null)
+                {
+                    stack.Push(root);
+                    root = root.left;
+                }
+                else
+                {
+                    root = stack.Pop();
+                    list.Add(root.val);
+                    root = root.right;
+                }
+            }
+            return list;
+        }
+    }
+}
