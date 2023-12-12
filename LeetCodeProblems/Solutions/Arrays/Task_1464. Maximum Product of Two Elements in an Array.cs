01f6dbb1fd49527a6410bd50f75df867722679e9
nevadimka <morozovvadimka@gmail.com>
2023-12-12 09:57:19 +0300

    Added LeetCode Problem #1464

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1464. Maximum Product of Two Elements in an Array.cs
@@ -0,0 +1,39 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1464
+    {
+        // #Array #Sorting #Heap(Priority Queue)
+        public int MaxProduct(int[] nums)
+        {
+            return Solution2(nums);
+        }
+
+        private int Solution1(int[] nums)
+        {
+            Array.Sort(nums);
+            return (nums[^1] - 1) * (nums[^2] - 1);
+        }
+
+        private int Solution2(int[] nums)
+        {
+            int maxA = 0, maxB = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= maxA)
+                {
+                    maxB = maxA;
+                    maxA = nums[i];
+                }
+                else
+                {
+                    if (nums[i] > maxB)
+                    {
+                        maxB = nums[i];
+                    }
+                }
+
+            }
+            return (maxA - 1) * (maxB - 1);
+        }
+    }
+}
