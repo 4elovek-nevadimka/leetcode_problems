da67e938c4e49576349fa601c8d47be13548e44c
nevadimka <morozovvadimka@gmail.com>
2023-12-15 11:30:52 +0300

    Added LeetCode Problem #1436

---
+++ LeetCodeProblems/Solutions/Task_1436. Destination City.cs
@@ -0,0 +1,50 @@
+﻿namespace Solutions
+{
+    internal class Task_1436
+    {
+        // #Hash Table #String
+        public string DestCity(IList<IList<string>> paths)
+        {
+            return Solution2(paths);
+        }
+
+        private string Solution1(IList<IList<string>> paths)
+        {
+            var hash = new HashSet<string>();
+            // add "from" cities
+            foreach (var path in paths)
+            {
+                hash.Add(path[0]);
+            }
+            // check "to" city in list
+            foreach (var path in paths)
+            {
+                if (!hash.Contains(path[1]))
+                    return path[1];
+            }
+            // impossible way
+            return string.Empty;
+        }
+
+        private string Solution2(IList<IList<string>> paths)
+        {
+            var destionationCity = paths[0][1];
+            var pathIndex = 0;
+
+            while (pathIndex < paths.Count)
+            {
+                if (destionationCity == paths[pathIndex][0])
+                {
+                    destionationCity = paths[pathIndex][1];
+                    pathIndex = 0;
+                }
+                else
+                {
+                    pathIndex++;
+                }
+            }
+
+            return destionationCity;
+        }
+    }
+}
