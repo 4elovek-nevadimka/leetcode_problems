eff60699b490f6e5f385d62c53ed273b84b13d7b
nevadimka <morozovvadimka@gmail.com>
2023-12-20 08:22:15 +0300

    Added LeetCode Problem #2706

---
+++ LeetCodeProblems/Solutions/Arrays/Task_2706. Buy Two Chocolates.cs
@@ -0,0 +1,38 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_2706
+    {
+        // #Array #Sorting
+        
+        public int BuyChoco(int[] prices, int money)
+        {
+            return Solution2(prices, money);
+        }
+
+        private int Solution1(int[] prices, int money)
+        {
+            Array.Sort(prices);
+            var leftover = money - prices[0] - prices[1];
+            return leftover < 0 ? money : leftover;
+        }
+
+        private int Solution2(int[] prices, int money)
+        {
+            int priceOne = 101, priceTwo = 101;
+            foreach (var price in prices)
+            {
+                if (price < priceOne)
+                {
+                    priceTwo = priceOne; priceOne = price;
+                }
+                else if (price < priceTwo)
+                {
+                    priceTwo = price;
+                }
+            }
+
+            var leftover = money - priceOne - priceTwo;
+            return leftover < 0 ? money : leftover;
+        }
+    }
+}
