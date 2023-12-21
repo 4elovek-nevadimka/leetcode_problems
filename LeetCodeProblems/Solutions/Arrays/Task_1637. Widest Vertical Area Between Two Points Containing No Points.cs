de92c527010b283a72a39c86206d31be86d754d3
nevadimka <morozovvadimka@gmail.com>
2023-12-21 11:46:17 +0300

    Added LeetCode Problem #1637

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1637. Widest Vertical Area Between Two Points Containing No Points.cs
@@ -0,0 +1,25 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1637
+    {
+        // #2dPlane
+        // #Array #Sorting
+
+        public int MaxWidthOfVerticalArea(int[][] points)
+        {
+            var n = points.Length;
+            var xCoordinates = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                xCoordinates[i] = points[i][0];
+            }
+            var maxDiff = 0;
+            Array.Sort(xCoordinates);
+            for (var i = 1; i < n; i++)
+            {
+                maxDiff = Math.Max(maxDiff, xCoordinates[i] - xCoordinates[i - 1]);
+            }
+            return maxDiff;
+        }
+    }
+}
