10b7244e1bfbe3672b73aa33cb087d718bba519f
nevadimka <morozovvadimka@gmail.com>
2023-12-22 09:26:05 +0300

    Added LeetCode Problem #1422

---
+++ LeetCodeProblems/Solutions/Strings/Task_1422. Maximum Score After Splitting a String.cs
@@ -0,0 +1,31 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_1422
+    {
+        // #String
+
+        public void Run()
+        {
+            Console.WriteLine(MaxScore("011101"));
+        }
+
+        public int MaxScore(string s)
+        {
+            var curScore = s[0] == '0' ? 1 : 0;
+            for (var i = 1; i < s.Length; i++)
+                if (s[i] == '1')
+                    curScore++;
+
+            var maxScore = curScore;
+            for (var i = 1; i < s.Length - 1; i++)
+            {
+                if (s[i] == '0')
+                    curScore++;
+                else
+                    curScore--;
+                maxScore = Math.Max(maxScore, curScore);
+            }
+            return maxScore;
+        }
+    }
+}
