9d9b399b98f82fd3c7e0e8a3c6b842379914cfbf
nevadimka <morozovvadimka@gmail.com>
2023-12-24 13:35:42 +0300

    Added LeetCode Problem #1758

---
+++ LeetCodeProblems/Solutions/Strings/Task_1758. Minimum Changes To Make Alternating Binary String.cs
@@ -0,0 +1,46 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_1758
+    {
+        // #String
+
+        public int MinOperations(string s)
+        {
+            return Solution2(s);
+        }
+
+        private int Solution1(string s)
+        {
+            return Math.Min(FlipCycle(s, '0'), FlipCycle(s, '1'));
+        }
+
+        private int FlipCycle(string s, char startWith)
+        {
+            var flipsCount = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (s[i] != startWith)
+                        flipsCount++;
+                }
+                else
+                {
+                    if (s[i] == startWith)
+                        flipsCount++;
+                }
+            }
+            return flipsCount;
+        }
+
+        private int Solution2(string s)
+        {
+            var flipsCount = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (i % 2 != s[i] - '0') flipsCount++;
+            }
+            return Math.Min(flipsCount, s.Length - flipsCount);
+        }
+    }
+}
