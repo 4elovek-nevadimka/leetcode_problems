1da65b08826afbb1381a502346265270e12b5e45
nevadimka <morozovvadimka@gmail.com>
2023-12-29 19:10:04 +0300

    Added LeetCode Problem #1335

---
+++ LeetCodeProblems/Solutions/DP/Task_1335. Minimum Difficulty of a Job Schedule.cs
@@ -0,0 +1,31 @@
+﻿namespace Solutions.DP
+{
+    internal class Task_1335
+    {
+        public int MinDifficulty(int[] jobDifficulty, int d)
+        {
+            var n = jobDifficulty.Length;
+            if (n < d) return -1;
+
+            var dp = new int[n + 1];
+            Array.Fill(dp, int.MaxValue / 2);
+            dp[n] = 0;
+
+            for (var day = 1; day <= d; day++)
+            {
+                for (var i = 0; i <= n - day; i++)
+                {
+                    var maxDifficulty = 0;
+                    dp[i] = int.MaxValue / 2;
+
+                    for (int j = i; j <= n - day; j++)
+                    {
+                        maxDifficulty = Math.Max(maxDifficulty, jobDifficulty[j]);
+                        dp[i] = Math.Min(dp[i], maxDifficulty + dp[j + 1]);
+                    }
+                }
+            }
+            return dp[0];
+        }
+    }
+}
