215064a8e19e4e162be51003204c448c78618804
nevadimka <morozovvadimka@gmail.com>
2023-12-30 20:52:20 +0300

    Added LeetCode Problem #1897

---
+++ LeetCodeProblems/Solutions/Strings/Task_1897. Redistribute Characters to Make All Strings Equal.cs
@@ -0,0 +1,36 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_1897
+    {
+        public bool MakeEqual(string[] words)
+        {
+            var dic = new Dictionary<char,  int>();
+            var n = words.Length;
+
+            foreach (var word in words)
+            {
+                foreach (var c in word)
+                {
+                    if (!dic.ContainsKey(c))
+                    {
+                        dic.Add(c, 1);
+                    }
+                    else
+                    {
+                        dic[c]++;
+                    }
+                }
+            }
+
+            foreach (var c in dic.Keys)
+            {
+                if (dic[c] % n != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
