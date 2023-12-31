4cc777114f63c0e255e5d14b658f7e96d9ded616
nevadimka <morozovvadimka@gmail.com>
2023-12-31 10:08:40 +0300

    Added LeetCode Problem #1624

---
+++ LeetCodeProblems/Solutions/Strings/Task_1624. Largest Substring Between Two Equal Characters.cs
@@ -0,0 +1,26 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_1624
+    {
+        // #Hash Table #String
+
+        public int MaxLengthBetweenEqualCharacters(string s)
+        {
+            var firstPositions = new Dictionary<char, int>();
+            var maxLengthBetween = -1;
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (!firstPositions.ContainsKey(s[i]))
+                {
+                    firstPositions.Add(s[i], i);
+                }
+                else
+                {
+                    maxLengthBetween = 
+                        Math.Max(maxLengthBetween, i - firstPositions[s[i]] - 1);
+                }
+            }
+            return maxLengthBetween;
+        }
+    }
+}
