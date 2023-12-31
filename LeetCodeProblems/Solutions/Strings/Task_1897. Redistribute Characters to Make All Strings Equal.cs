22e3a8840f8bf025a6fc50172634f2f597026d96
nevadimka <morozovvadimka@gmail.com>
2023-12-31 09:53:08 +0300

    Added one solution.

--- LeetCodeProblems/Solutions/Strings/Task_1897. Redistribute Characters to Make All Strings Equal.cs
+++ LeetCodeProblems/Solutions/Strings/Task_1897. Redistribute Characters to Make All Strings Equal.cs
@@ -4,7 +4,12 @@
     {
         public bool MakeEqual(string[] words)
         {
-            var dic = new Dictionary<char,  int>();
+            return Solution2(words);
+        }
+
+        private bool Solution1(string[] words)
+        {
+            var dic = new Dictionary<char, int>();
             var n = words.Length;
 
             foreach (var word in words)
@@ -25,6 +30,36 @@
             foreach (var c in dic.Keys)
             {
                 if (dic[c] % n != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool Solution2(string[] words)
+        {
+            var n = words.Length;
+            if (n == 1)
+                return true;
+
+            var totalChars = 0;
+            var alphabet = new int[26];
+            foreach (var word in words)
+            {
+                foreach (var c in word)
+                {
+                    alphabet[c - 'a']++;
+                    totalChars++;
+                }
+            }
+            if (totalChars % n != 0)
+                return false;
+            
+            foreach (var a in alphabet)
+            {
+                if (a % n != 0)
                 {
                     return false;
                 }
