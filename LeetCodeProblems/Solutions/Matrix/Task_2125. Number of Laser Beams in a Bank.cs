baec6de7d8fe9c999fbf2bf5a2497d3782efab97
nevadimka <morozovvadimka@gmail.com>
2024-01-03 19:38:04 +0300

    Added LeetCode Problem #2125

---
+++ LeetCodeProblems/Solutions/Matrix/Task_2125. Number of Laser Beams in a Bank.cs
@@ -0,0 +1,38 @@
+﻿namespace Solutions.Matrix
+{
+    internal class Task_2125
+    {
+        public int NumberOfBeams(string[] bank)
+        {
+            int rowOne = 0, rowTwo = 0;
+            int numberOfBeams = 0;
+
+            foreach (var c in bank[0])
+            {
+                if (c == '1')
+                {
+                    rowOne++;
+                }
+            }    
+
+            for (var i = 1; i < bank.Length; i++)
+            {
+                foreach (var c in bank[i])
+                {
+                    if (c == '1')
+                    {
+                        rowTwo++;
+                    }
+                }
+                if (rowTwo != 0)
+                {
+                    numberOfBeams += rowOne * rowTwo;
+                    rowOne = rowTwo;
+                    rowTwo = 0;
+                }
+            }
+
+            return numberOfBeams;
+        }
+    }
+}
