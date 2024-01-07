81d513cdd1cd1d5a4770884dc2c182af2aaaef17
nevadimka <morozovvadimka@gmail.com>
2024-01-07 18:51:44 +0300

    Added LeetCode Problem #0446

---
+++ LeetCodeProblems/Solutions/DP/Task_0446. Arithmetic Slices II - Subsequence.cs
@@ -0,0 +1,33 @@
+﻿namespace Solutions.DP
+{
+    internal class Task_0446
+    {
+        public int NumberOfArithmeticSlices(int[] nums)
+        {
+            var total = 0;
+            var n = nums.Length;
+            var dp = new Dictionary<int, int>[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                dp[i] = new Dictionary<int, int>();
+                for (var j = 0; j < i; j++)
+                {
+                    long longDiff = (long)nums[i] - nums[j];
+                    if (longDiff > int.MaxValue || longDiff < int.MinValue)
+                    {
+                        continue;
+                    }
+                    int diff = (int)longDiff;
+                    dp[i][diff] = dp[i].GetValueOrDefault(diff) + 1;
+                    if (dp[j].ContainsKey(diff))
+                    {
+                        dp[i][diff] += dp[j][diff];
+                        total += dp[j][diff];
+                    }
+                }
+            }
+            return total;
+        }
+    }
+}
