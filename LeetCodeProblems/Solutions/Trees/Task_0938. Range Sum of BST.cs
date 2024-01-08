ec52483fa614f4a9a862260c733c2363f962205b
nevadimka <morozovvadimka@gmail.com>
2024-01-08 12:48:16 +0300

    Added LeetCode Problem #0938

---
+++ LeetCodeProblems/Solutions/Trees/Task_0938. Range Sum of BST.cs
@@ -0,0 +1,18 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_0938
+    {
+        public int RangeSumBST(TreeNode root, int low, int high)
+        {
+            if (root == null)
+                return 0;
+            // Console.WriteLine(root.val);
+            var leftSum = 
+                root.val > low ? RangeSumBST(root.left, low, high) : 0;
+            var rightSum = 
+                root.val < high ? RangeSumBST(root.right, low, high) : 0;
+            return 
+                leftSum + rightSum + (root.val >= low && root.val <= high ? root.val : 0);
+        }
+    }
+}
