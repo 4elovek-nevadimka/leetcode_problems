a7f1892434f4f6b1fe719c3476a825f8b9db5e77
nevadimka <morozovvadimka@gmail.com>
2024-01-09 18:00:10 +0300

    Added LeetCode Problem #0872

---
+++ LeetCodeProblems/Solutions/Trees/Task_0872. Leaf-Similar Trees.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Solutions.Trees
+{
+    internal class Task_0872
+    {
+        // #Tree #Depth-First Search #Binary Tree
+
+        public bool LeafSimilar(TreeNode root1, TreeNode root2)
+        {
+            var leavesOne = new StringBuilder();
+            Traverse(root1, leavesOne);
+
+            var leavesTwo = new StringBuilder();
+            Traverse(root2, leavesTwo);
+
+            // Console.WriteLine(leavesOne.ToString() + " --> " + leavesTwo.ToString());
+            return leavesOne.Equals(leavesTwo);
+        }
+
+        private void Traverse(TreeNode node, StringBuilder leaves)
+        {
+            if (node == null)
+                return;
+
+            Traverse(node.left, leaves);
+
+            if (node.left == null && node.right == null)
+                leaves.Append(node.val).Append('.');
+
+            Traverse(node.right, leaves);
+        }
+    }
+}
