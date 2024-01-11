535add337587272d7d9895a7f8bf1735a70ba07b
nevadimka <morozovvadimka@gmail.com>
2024-01-11 19:16:08 +0300

    Added LeetCode Problem #1026

---
+++ LeetCodeProblems/Solutions/Trees/Task_1026. Maximum Difference Between Node and Ancestor.cs
@@ -0,0 +1,27 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_1026
+    {
+        public int MaxAncestorDiff(TreeNode root)
+        {
+            return Traverse(root, root.val, root.val);
+        }
+
+        private int Traverse(TreeNode node, int min, int max)
+        {
+            if (node == null)
+                return 0;
+
+            var current = 
+                Math.Max(Math.Abs(node.val - min), Math.Abs(node.val - max));
+
+            min = Math.Min(node.val, min);
+            max = Math.Max(node.val, max);
+
+            var left = Traverse(node.left, min, max);
+            var rigth = Traverse(node.right, min, max);
+
+            return Math.Max(current, Math.Max(left, rigth));
+        }
+    }
+}
