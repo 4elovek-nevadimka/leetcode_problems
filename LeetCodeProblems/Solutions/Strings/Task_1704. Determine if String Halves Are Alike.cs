b71d91c1735b7e55a8ba446f3a7d749f978e2707
nevadimka <morozovvadimka@gmail.com>
2024-01-12 12:11:03 +0300

    Added LeetCode Problem #1704

---
+++ LeetCodeProblems/Solutions/Strings/Task_1704. Determine if String Halves Are Alike.cs
@@ -0,0 +1,25 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_1704
+    {
+        public bool HalvesAreAlike(string s)
+        {
+            s = s.ToLower();
+            var n = s.Length;
+            int left = 0, right = 0;
+            for (var i = 0; i < n / 2; i++)
+            {
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                    left++;
+            }
+
+            for (var i = n - 1; i >= n / 2; i--)
+            {
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                    right++;
+            }
+
+            return left == right;
+        }
+    }
+}
