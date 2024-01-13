2ac2de10d9e2f0d0b8a68ca57ab4c562a3da962c
nevadimka <morozovvadimka@gmail.com>
2024-01-13 14:36:24 +0300

    Added LeetCode Problem #1512

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1512. Number of Good Pairs.cs
@@ -0,0 +1,66 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1512
+    {
+        // #Array #Hash Table #Math #Counting
+
+        public int NumIdenticalPairs(int[] nums)
+        {
+            return Solution2(nums);
+        }
+
+        private int Solution1(int[] nums)
+        {
+            var goodPairs = 0;
+            var pairs = new Dictionary<int, ICollection<int>>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (!pairs.ContainsKey(num))
+                {
+                    pairs.Add(num, new HashSet<int>());
+                }
+                pairs[num].Add(i);
+            }
+
+            foreach (int num in pairs.Keys)
+            {
+                var indexes = pairs[num];
+                for (var i = 0; i < indexes.Count; i++)
+                {
+                    for (var j = i + 1; j < indexes.Count; j++)
+                    {
+                        goodPairs++;
+                    }
+                }
+            }
+
+            return goodPairs;
+        }
+
+        private int Solution2(int[] nums)
+        {
+            var goodPairsCounter = 0;
+            var digitsCounter = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (!digitsCounter.ContainsKey(num))
+                {
+                    digitsCounter.Add(num, 1);
+                }
+                else
+                {
+                    digitsCounter[num]++;
+                }
+            }
+
+            foreach (int num in digitsCounter.Keys)
+            {
+                goodPairsCounter += 
+                    digitsCounter[num] * (digitsCounter[num] - 1) / 2;
+            }
+
+            return goodPairsCounter;
+        }
+    }
+}
