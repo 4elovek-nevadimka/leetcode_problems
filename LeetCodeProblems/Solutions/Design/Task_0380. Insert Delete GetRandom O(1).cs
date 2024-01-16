5600c6b45b4361674f4502c534a26d10b2cb827f
nevadimka <morozovvadimka@gmail.com>
2024-01-16 17:12:42 +0300

    Added LeetCode Problem #0380

---
+++ LeetCodeProblems/Solutions/Design/Task_0380. Insert Delete GetRandom O(1).cs
@@ -0,0 +1,47 @@
+﻿namespace Solutions.Design
+{
+    internal class Task_0380
+    {
+        public class RandomizedSet
+        {
+
+            private Random _random = new Random();
+            private List<int> _list = new List<int>();
+            private HashSet<int> _hash = new HashSet<int>();
+
+            public RandomizedSet()
+            {
+
+            }
+
+            public bool Insert(int val)
+            {
+                var result = _hash.Add(val);
+                if (result)
+                    _list.Add(val);
+                return result;
+            }
+
+            public bool Remove(int val)
+            {
+                var result = _hash.Remove(val);
+                if (result)
+                    _list.Remove(val);
+                return result;
+            }
+
+            public int GetRandom()
+            {
+                return _list[_random.Next(_list.Count)];
+            }
+        }
+
+        /**
+         * Your RandomizedSet object will be instantiated and called as such:
+         * RandomizedSet obj = new RandomizedSet();
+         * bool param_1 = obj.Insert(val);
+         * bool param_2 = obj.Remove(val);
+         * int param_3 = obj.GetRandom();
+         */
+    }
+}
