0fcbf7ea0c6edd50e2b0c7c8aad50975475a9d26
nevadimka <morozovvadimka@gmail.com>
2024-01-17 04:28:03 +0300

    Added LeetCode Problem #1207

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1207. Unique Number of Occurrences.cs
@@ -0,0 +1,26 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1207
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var num in arr)
+            {
+                if (!dic.ContainsKey(num))
+                    dic.Add(num, 1);
+                else
+                    dic[num]++;
+            }
+
+            var hash = new HashSet<int>();
+            foreach (var num in dic.Keys)
+            {
+                if (!hash.Add(dic[num]))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
