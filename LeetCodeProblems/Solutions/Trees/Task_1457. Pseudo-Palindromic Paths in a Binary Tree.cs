25e61afb079ac832be51d90d9317af42e6e52305
nevadimka <morozovvadimka@gmail.com>
2024-01-24 17:23:38 +0300

    Added LeetCode Problem #1457

---
+++ LeetCodeProblems/Solutions/Trees/Task_1457. Pseudo-Palindromic Paths in a Binary Tree.cs
@@ -0,0 +1,39 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_1457
+    {
+        // #Bit Manipulation #Tree #Depth-First Search #Breadth-First Search #Binary Tree
+        
+        private int ppPathsCounter = 0;
+
+        public int PseudoPalindromicPaths(TreeNode root)
+        {
+            DFS(root, new int[10]);
+            return ppPathsCounter;
+        }
+
+        private void DFS(TreeNode node, int[] freq)
+        {
+            freq[node.val]++;
+            if (node.left == null && node.right == null)
+            {
+                var oddCounter = 0;
+                for (var i = 1; i < freq.Length; i++)
+                {
+                    if (freq[i] % 2 != 0)
+                        oddCounter++;
+                }
+                if (oddCounter <= 1)
+                    ppPathsCounter++;
+            }
+
+            if (node.left != null)
+                DFS(node.left, freq);
+
+            if (node.right != null)
+                DFS(node.right, freq);
+
+            freq[node.val]--;
+        }
+    }
+}
