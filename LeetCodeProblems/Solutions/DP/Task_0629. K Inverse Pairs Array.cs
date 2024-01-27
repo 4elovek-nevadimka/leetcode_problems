7013e6ac918547e9e117f966e07f3caf5ea24bba
nevadimka <morozovvadimka@gmail.com>
2024-01-27 18:55:15 +0300

    Added LeetCode Problem #0629

---
+++ LeetCodeProblems/Solutions/DP/Task_0629. K Inverse Pairs Array.cs
@@ -0,0 +1,32 @@
+﻿namespace Solutions.DP
+{
+    class Task_0629
+    {
+		public int KInversePairs(int n, int k)
+		{
+			if (n == 0) return 0;
+
+			if (k == 0) return 1;
+
+			const int MOD = 1000000000 + 7;
+
+			var dp = new int[n + 1, k + 1];
+
+			for (var i = 1; i <= n; i++)
+			{
+				dp[i, 0] = 1;
+				for (var j = 1; j <= k; j++)
+				{
+					long tmp = 0;
+					for (var p = 0; p <= Math.Min(j, i - 1); p++)
+					{
+						tmp = (tmp + dp[i - 1, j - p]);
+					}
+					dp[i, j] = tmp % mod;
+				}
+			}
+
+			return dp[n, k];
+		}
+	}
+}
