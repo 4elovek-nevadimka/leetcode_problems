5805083a8c7be37c9a5f506df008e7f6cd09adfc
nevadimka <morozovvadimka@gmail.com>
2024-01-31 08:49:41 +0300

    Added LeetCode Problem #0739

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0739. Daily Temperatures.cs
@@ -0,0 +1,30 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0739
+    {
+        // #Array #Stack #Monotonic Stack
+
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            var n = temperatures.Length;
+            var answer = new int[n];
+
+            var stack = new Stack<int>();
+            stack.Push(0);
+            for (var i = 1; i < n; i++)
+            {
+                while (stack.Count > 0 &&
+                    temperatures[stack.Peek()] < temperatures[i])
+                {
+                    var index = stack.Pop();
+                    answer[index] = i - index;
+                }
+                stack.Push(i);
+            }
+            while (stack.Count > 0)
+                answer[stack.Pop()] = 0;
+
+            return answer;
+        }
+    }
+}
