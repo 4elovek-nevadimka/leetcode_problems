08d1ec3ab550ac7b0eed8d1a725a61fef5985819
nevadimka <morozovvadimka@gmail.com>
2024-02-03 19:36:17 +0300

    Added LeetCode Problem #1043

---
+++ LeetCodeProblems/Solutions/DP/Task_1043. Partition Array for Maximum Sum.cs
@@ -0,0 +1,26 @@
+﻿namespace Solutions.DP
+{
+    internal class Task_1043
+    {
+        public int MaxSumAfterPartitioning(int[] arr, int k)
+        {
+            var n = arr.Length;
+            var dp = new int[n + 1];
+
+            for (var i = 1; i <= n; i++)
+            {
+                var max = 0;
+                for (var j = 1; j <= k; j++)
+                {
+                    if (i - j >= 0)
+                    {
+                        max = Math.Max(max, arr[i - j]);
+                        dp[i] = Math.Max(dp[i], dp[i - j] + max * j);
+                    }
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
