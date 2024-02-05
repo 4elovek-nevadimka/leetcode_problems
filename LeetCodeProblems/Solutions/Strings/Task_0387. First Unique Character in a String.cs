6cb5a4951fe766489b41708864947e332ee3b0fb
nevadimka <morozovvadimka@gmail.com>
2024-02-05 10:23:40 +0300

    Added LeetCode Problem #0387

---
+++ LeetCodeProblems/Solutions/Strings/Task_0387. First Unique Character in a String.cs
@@ -0,0 +1,31 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_0387
+    {
+        // #Hash Table #String #Queue #Counting
+
+        public int FirstUniqChar(string s)
+        {
+            var alphabet = new int[26];
+            Array.Fill(alphabet, -1);
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var iChar = s[i] - 'a';
+                if (alphabet[iChar] == -1)
+                    alphabet[iChar] = i;
+                else
+                    alphabet[iChar] = -2;
+            }
+
+            var minxIndex = s.Length;
+            foreach (var val in alphabet)
+            {
+                if (val >= 0)
+                    minxIndex = Math.Min(minxIndex, val);
+            }
+
+            return minxIndex == s.Length ? -1 : minxIndex;
+        }
+    }
+}
