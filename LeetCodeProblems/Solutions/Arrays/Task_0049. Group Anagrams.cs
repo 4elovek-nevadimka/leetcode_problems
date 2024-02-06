33bce79c8286d2bac84b14a6184cfb3912350b8e
nevadimka <morozovvadimka@gmail.com>
2024-02-06 05:43:01 +0300

    Added LeetCode Problem #0049

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0049. Group Anagrams.cs
@@ -0,0 +1,28 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0049
+    {
+        // #Array #Hash Table #String #Sorting
+
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dic = new Dictionary<string, IList<string>>();
+            foreach (var str in strs)
+            {
+                var characters = str.ToArray();
+                Array.Sort(characters);
+                var sortedStr = new string(characters);
+
+                if (!dic.ContainsKey(sortedStr))
+                    dic.Add(sortedStr, new List<string>());
+                dic[sortedStr].Add(str);
+            }
+
+            var result = new List<IList<string>>();
+            foreach (var key in dic.Keys)
+                result.Add(dic[key]);
+
+            return result;
+        }
+    }
+}
