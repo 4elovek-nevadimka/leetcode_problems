9aa111d1727201aa822da1765bbbc139f7e98b71
nevadimka <morozovvadimka@gmail.com>
2024-02-06 06:05:06 +0300

    added slow solution

--- LeetCodeProblems/Solutions/Arrays/Task_0049. Group Anagrams.cs
+++ LeetCodeProblems/Solutions/Arrays/Task_0049. Group Anagrams.cs
@@ -6,6 +6,12 @@
 
         public IList<IList<string>> GroupAnagrams(string[] strs)
         {
+            return Solution1(strs);
+        }
+
+        // ok solution
+        private IList<IList<string>> Solution1(string[] strs)
+        {
             var dic = new Dictionary<string, IList<string>>();
             foreach (var str in strs)
             {
@@ -16,6 +22,40 @@
                 if (!dic.ContainsKey(sortedStr))
                     dic.Add(sortedStr, new List<string>());
                 dic[sortedStr].Add(str);
+            }
+
+            var result = new List<IList<string>>();
+            foreach (var key in dic.Keys)
+                result.Add(dic[key]);
+
+            return result;
+        }
+
+        // super slow solution
+        private IList<IList<string>> Solution2(string[] strs)
+        {
+            var dic = new Dictionary<int[], IList<string>>();
+            foreach (var str in strs)
+            {
+                var alphabet = new int[26];
+                for (var i = 0; i < str.Length; i++)
+                    alphabet[str[i] - 'a']++;
+
+                var exists = false;
+                foreach (var a in dic.Keys)
+                {
+                    if (a.SequenceEqual(alphabet))
+                    {
+                        dic[a].Add(str);
+                        exists = true;
+                        break;
+                    }
+                }
+                if (!exists)
+                {
+                    dic.Add(alphabet, new List<string>());
+                    dic[alphabet].Add(str);
+                }
             }
 
             var result = new List<IList<string>>();
