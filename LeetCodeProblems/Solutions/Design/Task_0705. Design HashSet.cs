a075c715f49b04d9fa0af1ce9b953035d0a631a3
nevadimka <morozovvadimka@gmail.com>
2024-02-08 20:02:54 +0300

    Added LeetCode Problem #0705

---
+++ LeetCodeProblems/Solutions/Design/Task_0705. Design HashSet.cs
@@ -0,0 +1,32 @@
+﻿namespace Solutions.Design
+{
+    internal class Task_0705
+    {
+        // #Array #Hash Table #Linked List #Design #Hash Function
+
+        internal class MyHashSet
+        {
+            private readonly bool[] _valuesMap;
+
+            public MyHashSet()
+            {
+                _valuesMap = new bool[1_000_001];
+            }
+
+            public void Add(int key)
+            {
+                _valuesMap[key] = true;
+            }
+
+            public void Remove(int key)
+            {
+                _valuesMap[key] = false;
+            }
+
+            public bool Contains(int key)
+            {
+                return _valuesMap[key];
+            }
+        }
+    }
+}
