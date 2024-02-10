ba7af36320755371aa84fd4d293da617cf3a2cad
nevadimka <morozovvadimka@gmail.com>
2024-02-10 19:20:57 +0300

    Added LeetCode Problem #0647

---
+++ LeetCodeProblems/Solutions/Strings/Task_0647. Palindromic Substrings.cs
@@ -0,0 +1,30 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_0647
+    {
+        // #String #Dynamic Programming
+
+        public int CountSubstrings(string s)
+        {
+            int count = 0, n = s.Length;
+
+            void ExpandAroundCenter(int left, int right)
+            {
+                while (left >= 0 && right < n && s[left] == s[right])
+                {
+                    count++;
+                    left--;
+                    right++;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                ExpandAroundCenter(i, i);
+                ExpandAroundCenter(i, i + 1);
+            }
+
+            return count;
+        }
+    }
+}
