2b9fa5f6249f02b4ee95fac2dcbf1c64bbeea125
nevadimka <morozovvadimka@gmail.com>
2024-02-12 09:41:47 +0300

    Added LeetCode Problem #0169

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0169. Majority Element.cs
@@ -0,0 +1,25 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0169
+    {
+        // #Array #Hash Table #Divide and Conquer #Sorting #Counting
+
+        public int MajorityElement(int[] nums)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (int x in nums)
+            {
+                if (!dic.ContainsKey(x))
+                    dic[x] = 1;
+                else
+                    dic[x]++;
+            }
+
+            foreach (var x in dic.Keys)
+                if (dic[x] > nums.Length / 2)
+                    return x;
+
+            return 0;
+        }
+    }
+}
