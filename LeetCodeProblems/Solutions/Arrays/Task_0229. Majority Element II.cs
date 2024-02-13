4df11eff3fdc2ae2c7e9783f78ce406701e75154
nevadimka <morozovvadimka@gmail.com>
2024-02-13 11:57:32 +0300

    Updated LeetCode Problem #0229 with Boyer Moore majority voting algorithm

--- LeetCodeProblems/Solutions/Arrays/Task_0229. Majority Element II.cs
+++ LeetCodeProblems/Solutions/Arrays/Task_0229. Majority Element II.cs
@@ -4,7 +4,23 @@
     {
         // #Array #Hash Table #Sorting #Counting
 
+        public void Run()
+        {
+            // expected 3
+            var nums = new[] { 3, 2, 3 };
+            // expected 0
+            // var nums = new[] { 0, 0, 0 };
+
+            var result = MajorityElement(nums);
+            Console.WriteLine(result);
+        }
+
         public IList<int> MajorityElement(int[] nums)
+        {
+            return Solution2(nums);
+        }
+
+        private IList<int> Solution1(int[] nums)
         {
             var appearTimes = nums.Length / 3;
             var appears = new Dictionary<int, int>();
@@ -24,13 +40,67 @@
             return appears.Where(x => x.Value > appearTimes).Select(x => x.Key).ToList();
         }
 
-        public void Run()
+        // Based on Boyer Moore majority voting algorithm
+        private IList<int> Solution2(int[] nums)
         {
-            // expected 3
-            var nums = new[] { 3, 2, 3 };
+            var k = 3;
+            var candidates = new int[k - 1];
+            var candidatesCount = new int[k - 1];
 
-            var result = MajorityElement(nums);
-            Console.WriteLine(result);
+            foreach (var num in nums)
+            {
+                var exists = false;
+                for (var i = 0; i < k - 1; i++)
+                {
+                    if (candidates[i] == num)
+                    {
+                        candidatesCount[i]++;
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (!exists)
+                {
+                    for (var i = 0; i < k - 1; i++)
+                    {
+                        if (candidatesCount[i] == 0)
+                        {
+                            candidates[i] = num;
+                            candidatesCount[i] = 1;
+                            exists = true;
+                            break;
+                        }
+                    }
+
+                    if (!exists)
+                    {
+                        for (var i = 0; i < k - 1; i++)
+                        {
+                            candidatesCount[i]--;
+                        }
+                    }
+                }
+            }
+
+            var result = new List<int>();
+            for (var i = 0; i < k - 1; i++)
+            {
+                if (result.Contains(candidates[i]))
+                    continue;
+
+                var count = 0;
+                foreach (var num in nums)
+                {
+                    if (num == candidates[i])
+                        count++;
+                }
+
+                if (count > nums.Length / k)
+                    result.Add(candidates[i]);
+            }
+
+            return result;
         }
     }
 }
--- LeetCodeProblems/Solutions/Program.cs
+++ LeetCodeProblems/Solutions/Program.cs
@@ -12,6 +12,6 @@ using Solutions.Trees;
 Console.WriteLine("LeetCode Problems!");
 
 // run task here for debugging
-new Task_0368().Run();
+new Task_0229().Run();
 
 // Console.ReadKey();
