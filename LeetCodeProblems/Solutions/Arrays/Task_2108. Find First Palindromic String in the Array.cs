f05a114fb0dd4c9ad0808e85371d3a7c599eeca2
nevadimka <morozovvadimka@gmail.com>
2024-02-13 09:33:00 +0300

    Added LeetCode Problem #2108

---
+++ LeetCodeProblems/Solutions/Arrays/Task_2108. Find First Palindromic String in the Array.cs
@@ -0,0 +1,28 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_2108
+    {
+        // #Array #Two Pointers #String
+
+        public string FirstPalindrome(string[] words)
+        {
+            foreach (var word in words)
+            {
+                int l = 0, r = word.Length - 1;
+                bool notEqual = false;
+                while (l < r)
+                {
+                    if (word[l++] != word[r--])
+                    {
+                        notEqual = true;
+                        break;
+                    }
+                }
+                if (!notEqual)
+                    return word;
+            }
+
+            return "";
+        }
+    }
+}
