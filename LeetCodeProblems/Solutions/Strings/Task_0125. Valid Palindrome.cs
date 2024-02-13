afca46cce638b3399589b8f22c0b41aaba93c8c2
nevadimka <morozovvadimka@gmail.com>
2024-02-13 09:59:43 +0300

    Added LeetCode Problem #0125

---
+++ LeetCodeProblems/Solutions/Strings/Task_0125. Valid Palindrome.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace Solutions.Strings
+{
+    internal class Task_0125
+    {
+        // #Two Pointers #String
+
+        public bool IsPalindrome(string s)
+        {
+            return Solution2(s);
+        }
+
+        private bool Solution1(string s)
+        {
+            var lettersOrDigits = new StringBuilder();
+            foreach (var c in s)
+                if (char.IsLetterOrDigit(c))
+                    lettersOrDigits.Append(c);
+
+            var formattedString = lettersOrDigits.ToString().ToLower();
+
+            int l = 0, r = formattedString.Length - 1;
+            while (l < r)
+            {
+                if (formattedString[l++] != formattedString[r--])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool Solution2(string s)
+        {
+            int l = 0, r = s.Length - 1;
+            while (l < r)
+            {
+                if (!char.IsLetterOrDigit(s[l]))
+                {
+                    l++;
+                    continue;
+                }
+                if (!char.IsLetterOrDigit(s[r]))
+                {
+                    r--;
+                    continue;
+                }
+
+                if (char.ToLower(s[l++]) != char.ToLower(s[r--]))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
