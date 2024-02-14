3eccad41b053778ba61a487223ac4062bbab1d7f
nevadimka <morozovvadimka@gmail.com>
2024-02-14 09:20:29 +0300

    Added LeetCode Problem #2149

---
+++ LeetCodeProblems/Solutions/Arrays/Task_2149. Rearrange Array Elements by Sign.cs
@@ -0,0 +1,27 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_2149
+    {
+        public int[] RearrangeArray(int[] nums)
+        {
+            var result = new int[nums.Length];
+            int posCounter = 0, negCounter = 1;
+
+            foreach (var num in nums)
+            {
+                if (num < 0)
+                {
+                    result[negCounter] = num;
+                    negCounter += 2;
+                }
+                else
+                {
+                    result[posCounter] = num;
+                    posCounter += 2;
+                }
+            }
+
+            return result;
+        }
+    }
+}
