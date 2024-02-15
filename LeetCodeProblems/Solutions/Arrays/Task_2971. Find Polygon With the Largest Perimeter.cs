ca6b80e179b1a24133fa110e5fd5091195bd1333
nevadimka <morozovvadimka@gmail.com>
2024-02-15 13:05:06 +0300

    Added LeetCode Problem #2971

---
+++ LeetCodeProblems/Solutions/Arrays/Task_2971. Find Polygon With the Largest Perimeter.cs
@@ -0,0 +1,51 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_2971
+    {
+        // #Array #Greedy #Sorting #Prefix Sum
+
+        public void Run()
+        {
+            // var nums = new[] { 1, 12, 1, 2, 5, 50, 3 };
+            var nums = new[] { 1, 11, 1, 2, 5, 14, 3 };
+            Console.WriteLine(LargestPerimeter(nums));
+        }
+
+        public long LargestPerimeter(int[] nums)
+        {
+            return Solution2(nums);
+        }
+
+        private long Solution1(int[] nums)
+        {
+            Array.Sort(nums);
+            var n = nums.Length;
+            var prefixSums = new long[n];
+            prefixSums[0] = nums[0];
+
+            for (var i = 1; i < n; i++)
+                prefixSums[i] = prefixSums[i - 1] + nums[i];
+
+            for (var i = n - 2; i > 0; i--)
+                if (prefixSums[i] > nums[i + 1])
+                    return prefixSums[i + 1];
+
+            return -1;
+        }
+
+        private long Solution2(int[] nums)
+        {
+            Array.Sort(nums);
+            long sum = nums[0], answer = -1;
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (sum - nums[i] > nums[i] && i > 1)
+                    answer = sum;
+            }
+
+            return answer;
+        }
+    }
+}
--- LeetCodeProblems/Solutions/Program.cs
+++ LeetCodeProblems/Solutions/Program.cs
@@ -12,6 +12,6 @@ using Solutions.Trees;
 Console.WriteLine("LeetCode Problems!");
 
 // run task here for debugging
-new Task_0229().Run();
+new Task_2971().Run();
 
 // Console.ReadKey();
