9607674b6ccf2e100204ae40fdca95bb0f8cfcf5
nevadimka <morozovvadimka@gmail.com>
2024-02-21 15:14:08 +0300

    Added LeetCode Problem #0201

---
+++ LeetCodeProblems/Solutions/BitManipulation/Task_0201. Bitwise AND of Numbers Range.cs
@@ -0,0 +1,15 @@
+﻿namespace Solutions.BitManipulation
+{
+    // #Bit Manipulation
+
+    internal class Task_0201
+    {
+        public int RangeBitwiseAnd(int left, int right)
+        {
+            while (left < right)
+                right &= (right - 1);
+
+            return left & right;
+        }
+    }
+}
