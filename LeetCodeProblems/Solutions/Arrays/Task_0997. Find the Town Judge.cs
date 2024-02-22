b0aa93114b21528014245bbfa07d8133412aed77
nevadimka <morozovvadimka@gmail.com>
2024-02-22 08:49:45 +0300

    Added LeetCode Problem #0997

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0997. Find the Town Judge.cs
@@ -0,0 +1,34 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0997
+    {
+        // #Array #Hash Table #Graph
+
+        public int FindJudge(int n, int[][] trust)
+        {
+            if (trust.Length == 0)
+                return n == 1 ? 1 : -1;
+
+            var people = new bool[n + 1];
+            var candidates = new int[n + 1];
+            foreach (var pair in trust)
+            {
+                people[pair[0]] = true;
+                candidates[pair[1]]++;
+            }
+
+            for (var i = 1; i <= n; i++)
+            {
+                if (!people[i])
+                {
+                    if (candidates[i] == n - 1)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
