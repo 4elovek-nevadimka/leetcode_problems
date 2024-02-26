ce3fe52fe2976ab8bcbf46eab59d221aada05251
nevadimka <morozovvadimka@gmail.com>
2024-02-26 16:42:02 +0300

    Added LeetCode Problem #0100

---
+++ LeetCodeProblems/Solutions/Trees/Task_0100. Same Tree.cs
@@ -0,0 +1,18 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_0100
+    {
+        // #Tree #Depth-First Search #Breadth-First Searc #Binary Tree
+
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+
+            if (p == null || q == null)
+                return false;
+
+            return p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
