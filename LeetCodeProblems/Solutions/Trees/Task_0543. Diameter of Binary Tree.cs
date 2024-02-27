527cf281117c5d963ab39870494297f7a5ecb659
nevadimka <morozovvadimka@gmail.com>
2024-02-27 12:48:23 +0300

    Added LeetCode Problem #0543

---
+++ LeetCodeProblems/Solutions/Trees/Task_0543. Diameter of Binary Tree.cs
@@ -0,0 +1,28 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_0543
+    {
+        // #Tree #Depth-First Search #Binary Tree
+
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            return TraverseTree(root).Diameter;
+        }
+
+        private (int Level, int Diameter) TraverseTree(TreeNode node)
+        {
+            if (node == null)
+                return (0, 0);
+
+            var left = TraverseTree(node.left);
+            var right = TraverseTree(node.right);
+
+            var level = Math.Max(left.Level, right.Level);
+            var diameter = Math.Max(left.Diameter, right.Diameter);
+
+            diameter = Math.Max(diameter, left.Level + right.Level);
+
+            return (level, diameter);
+        }
+    }
+}
