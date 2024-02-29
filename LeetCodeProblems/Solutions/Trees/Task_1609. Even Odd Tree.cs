505fc5ecc9bb796cc0c8418cc914bf58c4588a7b
nevadimka <morozovvadimka@gmail.com>
2024-02-29 22:12:35 +0300

    Added LeetCode Problem #1609

---
+++ LeetCodeProblems/Solutions/Trees/Task_1609. Even Odd Tree.cs
@@ -0,0 +1,47 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_1609
+    {
+        public bool IsEvenOddTree(TreeNode root)
+        {
+            if (root.val % 2 == 0)
+                return false;
+
+            var evenLevel = true;
+            var previousValue = 0;
+            var queue = new Queue<TreeNode>(new[] { root });
+
+            while (queue.Count > 0)
+            {
+                var n = queue.Count;
+                for (var i = 0; i < n; i++)
+                {
+                    var currentNode = queue.Dequeue();
+                    if (evenLevel)
+                    {
+                        if (currentNode.val % 2 == 0)
+                            return false;
+                        if (i > 0)
+                            if (currentNode.val <= previousValue)
+                                return false;
+                    }
+                    else
+                    {
+                        if (currentNode.val % 2 == 1)
+                            return false;
+                        if (i > 0)
+                            if (currentNode.val >= previousValue)
+                                return false;
+                    }
+                    if (currentNode.left != null)
+                        queue.Enqueue(currentNode.left);
+                    if (currentNode.right != null)
+                        queue.Enqueue(currentNode.right);
+                    previousValue = currentNode.val;
+                }
+                evenLevel = !evenLevel;
+            }
+            return true;
+        }
+    }
+}
