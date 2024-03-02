17ffc7ce14ec297522b0e8d597854907da8ac2ba
Nevadimka <morozovvadimka@gmail.com>
2024-03-03 00:26:20 +0300

    Added LeetCode Problem #0977

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0977. Squares of a Sorted Array.cs
@@ -0,0 +1,65 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0977
+    {
+        // #Array #Two Pointers #Sorting
+
+        public int[] SortedSquares(int[] nums)
+        {
+            return Solution2(nums);
+        }
+
+        private int[] Solution1(int[] nums)
+        {
+            var result = new int[nums.Length];
+            for (var i = 0; i < nums.Length; i++)
+                result[i] = nums[i] * nums[i];
+
+            Array.Sort(result);
+            return result;
+        }
+
+        private int[] Solution2(int[] nums)
+        {
+            var maxNum = 0;
+            // make positive and get max
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < 0)
+                    nums[i] = nums[i] * (-1);
+                if (nums[i] > maxNum)
+                    maxNum = nums[i];
+            }
+
+            var tmpArr = new int[maxNum + 1];
+            for (var i = 0; i < nums.Length; i++)
+                tmpArr[nums[i]]++;
+
+            var index = 0;
+            for (var i = 0; i < tmpArr.Length; i++)
+                for (var j = 0; j < tmpArr[i]; j++)
+                    nums[index++] = i * i;
+
+            return nums;
+        }
+
+        private int[] Solution3(int[] nums)
+        {
+            var maxNum = 0;
+            for (var i = 0; i < nums.Length; i++)
+                if (Math.Abs(nums[i]) > maxNum)
+                    maxNum = Math.Abs(nums[i]);
+
+            var tmpArr = new int[maxNum + 1];
+            for (var i = 0; i < nums.Length; i++)
+                tmpArr[Math.Abs(nums[i])]++;
+
+            var index = 0;
+            for (var i = 0; i < tmpArr.Length; i++)
+                for (var j = 0; j < tmpArr[i]; j++)
+                    nums[index++] = i * i;
+
+            return nums;
+        }
+    }
+}
