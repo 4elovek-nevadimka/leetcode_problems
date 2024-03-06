12b5df45b8eef3f2350044220dd408a2092cfc96
Nevadimka <morozovvadimka@gmail.com>
2024-03-06 20:52:48 +0300

    Added LeetCode Problem #0141

---
+++ LeetCodeProblems/Solutions/LinkedLists/Task_0141. Linked List Cycle.cs
@@ -0,0 +1,42 @@
+﻿namespace Solutions.LinkedLists
+{
+    internal class Task_0141
+    {
+        // #Hash Table #Linked List #Two Pointers
+
+        public bool HasCycle(ListNode head)
+        {
+            return Solution1(head);
+        }
+
+        private bool Solution1(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return false;
+
+            ListNode start = head, previous = null;
+            while (head.next != null)
+            {
+                var tmp = previous;
+                previous = head;
+                head = head.next;
+                previous.next = tmp;
+            }
+
+            return start == head;
+        }
+
+        private bool Solution2(ListNode head)
+        {
+            ListNode fast = head, slow = head;
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
