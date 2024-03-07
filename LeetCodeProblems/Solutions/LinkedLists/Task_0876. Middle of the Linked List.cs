72e3073af78c694863e2d2e4fe6a0c827a0cb341
Nevadimka <morozovvadimka@gmail.com>
2024-03-07 21:29:47 +0300

    Added LeetCode Problem #0876

---
+++ LeetCodeProblems/Solutions/LinkedLists/Task_0876. Middle of the Linked List.cs
@@ -0,0 +1,18 @@
+﻿namespace Solutions.LinkedLists
+{
+    internal class Task_0876
+    {
+        // #Linked List #Two Pointers
+
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode slow = head, fast = head;
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+}
