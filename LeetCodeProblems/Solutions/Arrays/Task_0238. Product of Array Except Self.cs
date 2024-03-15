67348dda0b8206989f1c046f165ce053d15295e9
Nevadimka <morozovvadimka@gmail.com>
2024-03-15 17:36:05 +0300

    Added LeetCode Problem #0238

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0238. Product of Array Except Self.cs
@@ -0,0 +1,29 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0238
+    {
+        // #Array #Prefix Sum
+
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var n = nums.Length;
+            var result = new int[n];
+
+            var leftProduct = 1;
+            for (var i = 0; i < n; i++)
+            {
+                result[i] = leftProduct;
+                leftProduct *= nums[i];
+            }
+
+            var rightProduct = 1;
+            for (var i = n - 1; i >= 0; i--)
+            {
+                result[i] *= rightProduct;
+                rightProduct *= nums[i];
+            }
+
+            return result;
+        }
+    }
+}
