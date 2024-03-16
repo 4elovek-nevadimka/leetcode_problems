e0f14a2f4001c0171dcd60b3b1ad82785d6096dc
Nevadimka <morozovvadimka@gmail.com>
2024-03-16 22:48:13 +0300

    Added LeetCode Problem #0525

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0525. Contiguous Array.cs
@@ -0,0 +1,33 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0525
+    {
+        // #Array #Hash Table #Prefix Sum
+
+        public void Run()
+        {
+            Console.WriteLine(FindMaxLength([1,1,0,0,1,1,0,1,1]));
+        }
+
+        public int FindMaxLength(int[] nums)
+        {
+            var maxLength = 0;
+            var n = nums.Length;
+            var sumArr = new int[n];
+            sumArr[0] = nums[0] == 0 ? -1 : 1;
+            var dic = new Dictionary<int, int> { { sumArr[0], 0 } };
+            for (var i = 1; i < n; i++)
+            {
+                sumArr[i] = sumArr[i - 1] + (nums[i] == 0 ? -1 : 1);
+                if (sumArr[i] == 0)
+                    maxLength = Math.Max(maxLength, i + 1);
+                else
+                    if (dic.TryGetValue(sumArr[i], out int value))
+                        maxLength = Math.Max(maxLength, i - value);
+                    else
+                        dic[sumArr[i]] = i;
+            }
+            return maxLength;
+        }
+    }
+}
--- LeetCodeProblems/Solutions/Program.cs
+++ LeetCodeProblems/Solutions/Program.cs
@@ -15,6 +15,6 @@ using Solutions.Trees;
 Console.WriteLine("LeetCode Problems!");
 
 // run task here for debugging
-new Task_0930().Run();
+new Task_0525().Run();
 
 // Console.ReadKey();
