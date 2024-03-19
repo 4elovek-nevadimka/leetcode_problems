4193cb08268c349cb0df48307d81dd9f93e4a822
Nevadimka <morozovvadimka@gmail.com>
2024-03-19 21:56:46 +0300

    Added LeetCode Problem #0621

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0621. Task Scheduler.cs
@@ -0,0 +1,32 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0621
+    {
+        // #Array #Hash Table #Greedy #Sorting #Heap(Priority Queue) #Counting
+
+        public void Run()
+        {
+            Console.WriteLine(LeastInterval(['A', 'C', 'A', 'B', 'D', 'B'], 1));
+        }
+
+        public int LeastInterval(char[] tasks, int n)
+        {
+            var tasksFreq = new int[26];
+            foreach (var taks in tasks)
+                tasksFreq[taks - 'A']++;
+
+            Array.Sort(tasksFreq);
+
+            var maxFreq = tasksFreq[25] - 1;
+            var idleStates = maxFreq * n;
+
+            for (var i = 24; i >= 0 && tasksFreq[i] > 0; i--)
+            {
+                idleStates -= Math.Min(maxFreq, tasksFreq[i]);
+            }
+
+            return idleStates > 0 ? idleStates + tasks.Length : tasks.Length;
+
+        }
+    }
+}
