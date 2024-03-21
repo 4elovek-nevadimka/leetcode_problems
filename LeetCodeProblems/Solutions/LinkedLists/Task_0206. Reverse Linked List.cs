23bb5d1cfe6a873be4498ba384a42c298b5e8415
Nevadimka <morozovvadimka@gmail.com>
2024-03-21 17:39:21 +0300

    Added LeetCode Problem #0206

---
+++ LeetCodeProblems/Solutions/LinkedLists/Task_0206. Reverse Linked List.cs
@@ -0,0 +1,48 @@
+﻿namespace Solutions.LinkedLists
+{
+    internal class Task_0206
+    {
+        // #Linked List #Recursion
+
+        public ListNode ReverseList(ListNode head)
+        {
+            return Solution1(head);
+        }
+
+        private ListNode Solution1(ListNode head)
+        {
+            if (head == null)
+                return null;
+
+            ListNode previous = null;
+            while (head.next != null)
+            {
+                var tmp = previous;
+                previous = head;
+                head = head.next;
+                previous.next = tmp;
+            }
+            head.next = previous;
+            return head;
+        }
+
+        //private ListNode Solution2(ListNode head)
+        //{
+        //    if (head == null)
+        //        return null;
+        //    return Reverse(head, null);
+        //}
+
+        //private ListNode Reverse(ListNode node, ListNode prev)
+        //{
+        //    if (node.next == null)
+        //    {
+        //        node.next = prev;
+        //        return node;
+        //    }
+
+        //    prev?.next = node;
+        //    return Reverse(node.next, node);
+        //}
+    }
+}
