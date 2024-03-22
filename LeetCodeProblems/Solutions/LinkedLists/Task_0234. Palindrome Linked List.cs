d61d53fbe71c8d2e390b18724e1fc38d5602dd1b
Nevadimka <morozovvadimka@gmail.com>
2024-03-22 17:13:28 +0300

    Updated LeetCode Problem #0234

--- LeetCodeProblems/Solutions/LinkedLists/Task_0234. Palindrome Linked List.cs
+++ LeetCodeProblems/Solutions/LinkedLists/Task_0234. Palindrome Linked List.cs
@@ -9,9 +9,15 @@
             Console.WriteLine(IsPalindrome(ListNodeGenerator.Generate([1, 2])));
             Console.WriteLine(IsPalindrome(ListNodeGenerator.Generate([1, 2, 2, 1])));
             Console.WriteLine(IsPalindrome(ListNodeGenerator.Generate([1, 2, 3, 2, 1])));
+            Console.WriteLine(IsPalindrome(ListNodeGenerator.Generate([1, 6, 1, 1])));
         }
 
         public bool IsPalindrome(ListNode head)
+        {
+            return Solution2(head);
+        }
+
+        private bool Solution1(ListNode head)
         {
             var stack = new Stack<int>();
             ListNode fast = head, slow = head;
@@ -32,6 +38,43 @@
                 slow = slow.next;
             }
             return stack.Count == 0 && slow == null;
+        }
+
+        private bool Solution2(ListNode head)
+        {
+            ListNode fast = head, slow = head;
+            while (fast.next != null && fast.next.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            if (fast.next != null)
+            {
+                var tmp = slow;
+                slow = slow.next;
+                tmp.next = null;
+            }
+
+            slow = ReverseList(slow);
+            while (slow != null && head != null)
+            {
+                if (slow.val != head.val)
+                    return false;
+                slow = slow.next;
+                head = head.next;
+            }
+            return slow == null && head == null;
+        }
+
+        private ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode tmp = ReverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return tmp;
         }
     }
 }
