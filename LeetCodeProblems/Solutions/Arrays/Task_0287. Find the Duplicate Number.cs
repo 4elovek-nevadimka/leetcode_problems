953e3b46d93bf4478b15c46ef5a7e316d20055dd
Nevadimka <morozovvadimka@gmail.com>
2024-03-24 08:36:16 +0300

    Added LeetCode Problem #0287

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0287. Find the Duplicate Number.cs
@@ -0,0 +1,25 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0287
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            // #Array #Two Pointers #Binary Search #Bit Manipulation
+
+            int tortoise = nums[0], hare = nums[0];
+            do
+            {
+                tortoise = nums[tortoise];
+                hare = nums[nums[hare]];
+            } while (tortoise != hare);
+
+            tortoise = nums[0];
+            while (tortoise != hare)
+            {
+                tortoise = nums[tortoise];
+                hare = nums[hare];
+            }
+            return tortoise;
+        }
+    }
+}
