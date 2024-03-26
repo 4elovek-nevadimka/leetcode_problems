db32a1e0b0e2f78c55aecc6faaed136da81c90e1
Nevadimka <morozovvadimka@gmail.com>
2024-03-26 18:12:59 +0300

    Added LeetCode Problem #0041

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0041. First Missing Positive.cs
@@ -0,0 +1,36 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0041
+    {
+        // #Array
+
+        public void Run()
+        {
+            Console.WriteLine(FirstMissingPositive([1, 2, 0]));
+            Console.WriteLine(FirstMissingPositive([3, 4, -1, 1]));
+            Console.WriteLine(FirstMissingPositive([7, 8, 9, 11, 12]));
+        }
+
+        public int FirstMissingPositive(int[] nums)
+        {
+            var n = nums.Length;
+            for (var i = 0; i < n; i++)
+                if (nums[i] <= 0 || nums[i] > n)
+                    nums[i] = n + 1;
+
+            for (var i = 0; i < n; i++)
+            {
+                var index = Math.Abs(nums[i]);
+                if (index <= n)
+                    if (nums[index - 1] > 0)
+                        nums[index - 1] = -nums[index - 1];
+            }
+
+            for (var i = 0; i < n; i++)
+                if (nums[i] > 0)
+                    return i + 1;
+
+            return n + 1;
+        }
+    }
+}
--- LeetCodeProblems/Solutions/Program.cs
+++ LeetCodeProblems/Solutions/Program.cs
@@ -15,6 +15,6 @@ using Solutions.Trees;
 Console.WriteLine("LeetCode Problems!");
 
 // run task here for debugging
-new Task_0442().Run();
+new Task_0041().Run();
 
 // Console.ReadKey();
