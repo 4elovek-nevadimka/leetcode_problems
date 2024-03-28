2e5e557a5307aa8d67e5b1453e9c8c67b5b18f12
Nevadimka <morozovvadimka@gmail.com>
2024-03-28 12:27:17 +0300

    Added LeetCode Problem #2958

---
+++ LeetCodeProblems/Solutions/Arrays/Task_2958. Length of Longest Subarray With at Most K Frequency.cs
@@ -0,0 +1,44 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_2958
+    {
+        // #Array #Hash Table #Sliding Window
+
+        public void Run()
+        {
+            Console.WriteLine(MaxSubarrayLength([1, 2, 3, 1, 2, 3, 1, 2], 2));
+        }
+
+        public int MaxSubarrayLength(int[] nums, int k)
+        {
+            int left = 0, maxLength = 0;
+            var kHash = new HashSet<int>();
+            var freqDic = new Dictionary<int, int>();
+
+            for (var right = 0; right < nums.Length; right++)
+            {
+                var num = nums[right];
+                if (freqDic.TryGetValue(num, out int value))
+                    freqDic[num] = ++value;
+                else
+                    freqDic[num] = 1;
+
+                if (freqDic[num] > k)
+                    kHash.Add(num);
+
+                while (kHash.Count > 0 && left <= right)
+                {
+                    num = nums[left];
+                    freqDic[num]--;
+                    if (freqDic[num] <= k)
+                        kHash.Remove(num);
+                    
+                    left++;
+                }
+                maxLength = Math.Max(maxLength, right - left + 1);
+            }
+
+            return maxLength;
+        }
+    }
+}
