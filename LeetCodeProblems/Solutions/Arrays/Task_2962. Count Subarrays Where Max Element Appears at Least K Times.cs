9060cb38f4401a1630dc9540985ee635a18561ae
Nevadimka <morozovvadimka@gmail.com>
2024-03-29 19:37:31 +0300

    Added LeetCode Problem #2962

---
+++ LeetCodeProblems/Solutions/Arrays/Task_2962. Count Subarrays Where Max Element Appears at Least K Times.cs
@@ -0,0 +1,30 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_2962
+    {
+        // #Array #Sliding Window
+
+        public long CountSubarrays(int[] nums, int k)
+        {
+            var maxNum = 0;
+            foreach (var num in nums)
+                maxNum = Math.Max(maxNum, num);
+
+            long result = 0;
+            int left = 0, maxLength = 0;
+            for (var right = 0; right < nums.Length; right++)
+            {
+                if (nums[right] == maxNum)
+                    maxLength++;
+
+                while (k == maxLength)
+                    if (nums[left++] == maxNum)
+                        maxLength--;
+
+                result += left;
+            }
+
+            return result;   
+        }
+    }
+}
