b5a06f2e27df7f94985e06a7d3daaea91ba3e874
Nevadimka <morozovvadimka@gmail.com>
2024-03-30 17:58:50 +0300

    Added LeetCode Problem #0992

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0992. Subarrays with K Different Integers.cs
@@ -0,0 +1,40 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0992
+    {
+        // #Array #Hash Table #Sliding Window #Counting
+
+        public int SubarraysWithKDistinct(int[] nums, int k)
+        {
+            return Foo(nums, k) - Foo(nums, k - 1);
+        }
+
+        private int Foo(int[] nums, int k)
+        {
+            int left = 0, total = 0;
+            var freqDic = new Dictionary<int, int>();
+
+            for (var right = 0; right < nums.Length; right++)
+            {
+                var num = nums[right];
+                if (freqDic.TryGetValue(num, out int value))
+                    freqDic[num] = ++value;
+                else
+                    freqDic[num] = 1;
+
+                while (freqDic.Count > k)
+                {
+                    num = nums[left];
+                    freqDic[num]--;
+                    if (freqDic[num] == 0)
+                        freqDic.Remove(num);
+
+                    left++;
+                }
+                total += right - left + 1;
+            }
+
+            return total;
+        }
+    }
+}
