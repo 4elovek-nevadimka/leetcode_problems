f76f7b0fb227b6e650cd91d10655b1144d607803
Nevadimka <morozovvadimka@gmail.com>
2024-04-01 05:50:33 +0300

    Added LeetCode Problem #0058

---
+++ LeetCodeProblems/Solutions/Strings/Task_0058. Length of Last Word.cs
@@ -0,0 +1,34 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_0058
+    {
+        // #String
+
+        public int LengthOfLastWord(string s)
+        {
+            return Solution1(s);
+        }
+
+        private int Solution1(string s)
+        {
+            return s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Last().Length;
+        }
+
+        private int Solution2(string s)
+        {
+            int index = s.Length - 1;
+            while (s[index] == ' ')
+                index--;
+
+            int lastCharIndex = index;
+            while (index > 0)
+                if (s[--index] == ' ')
+                    break;
+
+            if (index == 0 && s[index] != ' ')
+                index--;
+
+            return lastCharIndex - index;
+        }
+    }
+}
