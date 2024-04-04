540a0747391bfc4930f59c86b1c9db22a0788d21
Nevadimka <morozovvadimka@gmail.com>
2024-04-04 15:14:46 +0300

    Added LeetCode Problem #1614

---
+++ LeetCodeProblems/Solutions/Strings/Task_1614. Maximum Nesting Depth of the Parentheses.cs
@@ -0,0 +1,24 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_1614
+    {
+        // #String #Stack
+
+        public int MaxDepth(string s)
+        {
+            int depth = 0, maxDepth = 0;
+            foreach (var c in s)
+            {
+                if (c == '(')
+                {
+                    depth++;
+                    if (depth > maxDepth)
+                        maxDepth = depth;
+                }
+                else if (c == ')')
+                    depth--;
+            }
+            return maxDepth;
+        }
+    }
+}
