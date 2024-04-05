07f398d23803b40911f9e348db8cbc7134569eba
Nevadimka <morozovvadimka@gmail.com>
2024-04-05 05:51:22 +0300

    Added one more solution

--- LeetCodeProblems/Solutions/Strings/Task_1544. Make The String Great.cs
+++ LeetCodeProblems/Solutions/Strings/Task_1544. Make The String Great.cs
@@ -15,6 +15,11 @@ namespace Solutions.Strings
 
         public string MakeGood(string s)
         {
+            return Solution2(s);
+        }
+
+        private string Solution1(string s)
+        {
             var n = s.Length;
             var goodString = new StringBuilder();
             for (var i = 0; i < n; i++)
@@ -25,6 +30,22 @@ namespace Solutions.Strings
 
             if (goodString.Length != n)
                 return MakeGood(goodString.ToString());
+
+            return goodString.ToString();
+        }
+
+        private string Solution2(string s)
+        {
+            var n = s.Length;
+            var goodString = new StringBuilder();
+            for (var i = 0; i < n; i++)
+                if ((i < n - 1) && (Math.Abs(s[i] - s[i + 1]) == 32))
+                    i++;
+                else
+                    if ((goodString.Length > 0) && (Math.Abs(s[i] - goodString[^1]) == 32))
+                        goodString.Remove(goodString.Length - 1, 1);
+                    else
+                        goodString.Append(s[i]);
 
             return goodString.ToString();
         }
