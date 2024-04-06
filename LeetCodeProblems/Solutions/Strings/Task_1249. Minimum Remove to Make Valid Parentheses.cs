1b5795b5821a9e1abddfd432e32aa75d5c558465
Nevadimka <morozovvadimka@gmail.com>
2024-04-06 20:42:18 +0300

    Added LeetCode Problem #1249

---
+++ LeetCodeProblems/Solutions/Strings/Task_1249. Minimum Remove to Make Valid Parentheses.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace Solutions.Strings
+{
+    internal class Task_1249
+    {
+        // #String #Stack
+
+        public void Run()
+        {
+            Console.WriteLine(MinRemoveToMakeValid("lee(t(c)o)de)"));
+            Console.WriteLine(MinRemoveToMakeValid("a)b(c)d"));
+            Console.WriteLine(MinRemoveToMakeValid("))(("));
+            Console.WriteLine(MinRemoveToMakeValid("())()((("));
+        }
+
+        public string MinRemoveToMakeValid(string s)
+        {
+            var stack = new Stack<int>();
+            var goodString = new StringBuilder(s);
+
+            for (var i = 0; i < goodString.Length; i++)
+            {
+                if (goodString[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (goodString[i] == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        goodString[i] = '-';
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                goodString[stack.Pop()] = '-';
+            }
+
+            return goodString.ToString().Replace("-", "");
+        }
+    }
+}
