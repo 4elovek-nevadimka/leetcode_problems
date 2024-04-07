6998a7dec798c2593464ffe1829758a00beb0d5f
Nevadimka <morozovvadimka@gmail.com>
2024-04-07 16:15:24 +0300

    Added LeetCode Problem #0678

---
+++ LeetCodeProblems/Solutions/Strings/Task_0678. Valid Parenthesis String.cs
@@ -0,0 +1,36 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_0678
+    {
+        // #String #Dynamic Programming #Stack #Greedy
+
+        public bool CheckValidString(string s)
+        {
+            int opens = 0, closes = 0;
+            foreach (char c in s)
+            {
+                if (c == '(')
+                {
+                    opens++;
+                    closes++;
+                }
+                else if (c == ')')
+                {
+                    if (opens > 0)
+                        opens--;
+                    closes--;
+                    if (closes < 0)
+                        return false;
+                }
+                else
+                {
+                    if (opens > 0)
+                        opens--;
+                    closes++;
+                }
+            }
+
+            return opens == 0;
+        }
+    }
+}
