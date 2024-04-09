a0d6f8fbc1647e25180e67a298d81bec8bb3a54f
Nevadimka <morozovvadimka@gmail.com>
2024-04-09 17:29:31 +0300

    Added LeetCode Problem #2073

---
+++ LeetCodeProblems/Solutions/Simulation/Task_2073. Time Needed to Buy Tickets.cs
@@ -0,0 +1,27 @@
+﻿namespace Solutions.Simulation
+{
+    internal class Task_2073
+    {
+        // #Array #Queue #Simulation
+
+        public int TimeRequiredToBuy(int[] tickets, int k)
+        {
+            var time = 0;
+            while (tickets[k] > 0)
+            {
+                for (var i = 0; i < tickets.Length; i++)
+                {
+                    if (tickets[i] > 0)
+                    {
+                        time++;
+                        tickets[i]--;
+                    }
+                    if (i == k && tickets[i] == 0)
+                        break;
+                }
+            }
+
+            return time;
+        }
+    }
+}
