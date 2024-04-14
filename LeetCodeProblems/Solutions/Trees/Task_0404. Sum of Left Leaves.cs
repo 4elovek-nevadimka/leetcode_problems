4a6bb5447418c6e228321a5103de5daf108089ce
Nevadimka <morozovvadimka@gmail.com>
2024-04-14 21:12:45 +0300

    Added LeetCode Problem #0404

---
+++ LeetCodeProblems/Solutions/Trees/Task_0404. Sum of Left Leaves.cs
@@ -0,0 +1,25 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_0404
+    {
+        // #Tree #Depth-First Search #Breadth-First Search #Binary Tree
+
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            return SumOfLeftLeavesTraverse(root, false);
+        }
+
+        private int SumOfLeftLeavesTraverse(TreeNode node, bool isLeft)
+        {
+            if (node == null) return 0;
+
+            if (node.left == null && node.right == null && isLeft)
+                return node.val;
+
+            var leftSum = SumOfLeftLeavesTraverse(node.left, true);
+            var rightSum = SumOfLeftLeavesTraverse(node.right, false);
+
+            return leftSum + rightSum;
+        }
+    }
+}
