169da6ea1c6b3432376507588cce71ddd95a8722
Nevadimka <morozovvadimka@gmail.com>
2024-04-15 16:46:50 +0300

    Added LeetCode Problem #0129

---
+++ LeetCodeProblems/Solutions/Trees/Task_0129. Sum Root to Leaf Numbers.cs
@@ -0,0 +1,30 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_0129
+    {
+        // #Tree #Depth-First Search #Binary Tree
+
+        public int SumNumbers(TreeNode root)
+        {
+            return SumNumbersTraverse(root, root.val);
+        }
+
+        private int SumNumbersTraverse(TreeNode node, int value)
+        {
+            if (node == null)
+                return 0;
+
+            var curValue = value * 10 + node.val;
+
+            if (node.left == null && node.right == null)
+            {
+                return curValue;
+            }
+
+            var left = SumNumbersTraverse(node.left, curValue);
+            var right = SumNumbersTraverse(node.right, curValue);
+
+            return left + right;
+        }
+    }
+}
