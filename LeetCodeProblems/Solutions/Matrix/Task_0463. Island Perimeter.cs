e1db93d3c0376f0241b8506bb1b2ebe1cf4ac328
Nevadimka <morozovvadimka@gmail.com>
2024-04-18 21:03:05 +0300

    Added LeetCode Problem #0463

---
+++ LeetCodeProblems/Solutions/Matrix/Task_0463. Island Perimeter.cs
@@ -0,0 +1,29 @@
+﻿namespace Solutions.Matrix
+{
+    internal class Task_0463
+    {
+        // #Array #Depth-First Search #Breadth-First Search #Matrix
+
+        public int IslandPerimeter(int[][] grid)
+        {
+            var perimeter = 0;
+            for (var i = 0; i < grid.Length; i++)
+            {
+                for (var j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        perimeter += 4;
+
+                        if (i > 0 && grid[i - 1][j] == 1)
+                            perimeter -= 2;
+                        if (j > 0 && grid[i][j - 1] == 1)
+                            perimeter -= 2;
+                    }
+                }
+            }
+
+            return perimeter;
+        }
+    }
+}
