93d82b94a1458b7952a1371792bdf8ff78307f3e
Nevadimka <morozovvadimka@gmail.com>
2024-04-20 21:48:30 +0300

    Added LeetCode Problem #1992

---
+++ LeetCodeProblems/Solutions/Matrix/Task_1992. Find All Groups of Farmland.cs
@@ -0,0 +1,47 @@
+﻿namespace Solutions.Matrix
+{
+    internal class Task_1992
+    {
+        // #Array #Depth-First Search #Breadth-First Search #Matrix
+
+        public int[][] FindFarmland(int[][] land)
+        {
+            var farmland = new List<int[]>();
+            int m = land.Length, n = land[0].Length;
+
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    if (land[i][j] == 1)
+                    {
+                        var bottomRight = new int[2];
+                        DFS(land, i, j, bottomRight);
+                        farmland.Add([i, j, bottomRight[0], bottomRight[1]]);
+                    }
+                }
+            }
+
+            return [.. farmland];
+        }
+
+        private void DFS(int[][] land, int row, int col, int[] bottomRight)
+        {
+            if (row < 0 || row >= land.Length
+                || col < 0 || col >= land[0].Length || land[row][col] == 0)
+            {
+                return;
+            }
+
+            bottomRight[0] = Math.Max(bottomRight[0], row);
+            bottomRight[1] = Math.Max(bottomRight[1], col);
+
+            land[row][col] = 0;
+
+            DFS(land, row - 1, col, bottomRight);
+            DFS(land, row + 1, col, bottomRight);
+            DFS(land, row, col - 1, bottomRight);
+            DFS(land, row, col + 1, bottomRight);
+        }
+    }
+}
