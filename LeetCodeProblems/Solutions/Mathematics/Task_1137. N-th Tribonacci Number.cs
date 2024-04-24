044285eb286633c3f8c9c0970bb2ce4f52751096
Nevadimka <morozovvadimka@gmail.com>
2024-04-24 04:58:04 +0300

    Added LeetCode Problem #1137

---
+++ LeetCodeProblems/Solutions/Mathematics/Task_1137. N-th Tribonacci Number.cs
@@ -0,0 +1,24 @@
+﻿namespace Solutions.Mathematics
+{
+    internal class Task_1137
+    {
+        // #Math #Dynamic Programming #Memoization
+
+        public int Tribonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1 || n == 2)
+                return 1;
+
+            var arr = new int[n + 1];
+            arr[0] = 0; arr[1] = arr[2] = 1;
+            for (var i = 3; i < n + 1; i++)
+            {
+                arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1];
+            }
+
+            return arr[n];
+        }
+    }
+}
