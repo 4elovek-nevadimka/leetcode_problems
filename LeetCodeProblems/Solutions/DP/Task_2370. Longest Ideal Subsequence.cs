f5481ece4da1a08ad308f25888b60569b1cd1448
Nevadimka <morozovvadimka@gmail.com>
2024-04-25 18:00:06 +0300

    Added LeetCode Problem #2370

---
+++ LeetCodeProblems/Solutions/DP/Task_2370. Longest Ideal Subsequence.cs
@@ -0,0 +1,34 @@
+﻿namespace Solutions.DP
+{
+    internal class Task_2370
+    {
+        // #Hash Table #String #Dynamic Programming
+
+        public int LongestIdealString(string s, int k)
+        {
+            var n = s.Length;
+            var dp = new int[26];
+            var longestIdealString = 0;
+
+            for (var i = 0; i < n; i++)
+            {
+                var curr = s[i];
+                var idx = curr - 'a';
+                var longest = 0;
+
+                for (var prev = 0; prev < 26; prev++)
+                {
+                    if (Math.Abs(curr - 'a' - prev) <= k)
+                    {
+                        longest = Math.Max(longest, dp[prev]);
+                    }
+                }
+
+                dp[idx] = longest + 1;
+                longestIdealString = Math.Max(longestIdealString, dp[idx]);
+            }
+
+            return longestIdealString;
+        }
+    }
+}
