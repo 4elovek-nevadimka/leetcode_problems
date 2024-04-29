2df908f45648fb6598bb1aefb99e8278791bb531
Nevadimka <morozovvadimka@gmail.com>
2024-04-29 19:36:00 +0300

    Added LeetCode Problem #2997

---
+++ LeetCodeProblems/Solutions/BitManipulation/Task_2997. Minimum Number of Operations to Make Array XOR Equal to K.cs
@@ -0,0 +1,27 @@
+﻿namespace Solutions.BitManipulation
+{
+    internal class Task_2997
+    {
+        // #Array #Bit Manipulation
+
+        public int MinOperations(int[] nums, int k)
+        {
+            var xor = 0;
+            foreach (var num in nums)
+                xor ^= num;
+
+            if (xor == k)
+                return 0;
+
+            var counter = 0;
+            var xorK = xor ^ k;
+            while (xorK != 0)
+            {
+                counter += xorK & 1;
+                xorK >>= 1;
+            }
+
+            return counter;
+        }
+    }
+}
