d1e2b503806de9659e890a5b0a4289cd8b1626d2
Nevadimka <morozovvadimka@gmail.com>
2024-04-30 21:25:09 +0300

    Added LeetCode Problem #1915

---
+++ LeetCodeProblems/Solutions/BitManipulation/Task_1915. Number of Wonderful Substrings.cs
@@ -0,0 +1,31 @@
+﻿namespace Solutions.BitManipulation
+{
+    // #Hash Table #String #Bit Manipulation #Prefix Sum
+
+    internal class Task_1915
+    {
+        public long WonderfulSubstrings(string word)
+        {
+            var dic = new Dictionary<int, long>();
+            dic[0] = 1;
+            long result = 0;
+            int mask = 0;
+
+            foreach (char c in word)
+            {
+                mask ^= 1 << (c - 'a');
+                result += dic.GetValueOrDefault(mask, 0);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    int checkMask = mask ^ (1 << i);
+                    result += dic.GetValueOrDefault(checkMask, 0);
+                }
+
+                dic[mask] = dic.GetValueOrDefault(mask, 0) + 1;
+            }
+
+            return result;
+        }
+    }
+}
