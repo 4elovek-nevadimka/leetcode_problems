9038f499d4ef8b406a2ea463e75c03daf30cfc5c
Nevadimka <morozovvadimka@gmail.com>
2024-05-01 22:17:30 +0300

    Added LeetCode Problem #2000

--- LeetCodeProblems/Solutions/Program.cs
+++ LeetCodeProblems/Solutions/Program.cs
@@ -15,6 +15,6 @@ using Solutions.Trees;
 Console.WriteLine("LeetCode Problems!");
 
 // run task here for debugging
-new Task_0988().Run();
+new Task_2000().Run();
 
 // Console.ReadKey();
---
+++ LeetCodeProblems/Solutions/Strings/Task_2000. Reverse Prefix of Word.cs
@@ -0,0 +1,26 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_2000
+    {
+        // #Two Pointers #String
+
+        public void Run()
+        {
+            Console.WriteLine(ReversePrefix("xyxzxe", 'z'));
+        }
+
+        public string ReversePrefix(string word, char ch)
+        {
+            if (!word.Contains(ch))
+                return word;
+
+            var index = word.IndexOf(ch);
+            var charArr = index == word.Length - 1 
+                ? word.ToCharArray() : word[..(index + 1)].ToCharArray();
+            Array.Reverse(charArr);
+
+            return index == word.Length - 1 ? 
+                new string(charArr) : string.Concat(new string(charArr), word.AsSpan(index + 1));
+        }
+    }
+}
