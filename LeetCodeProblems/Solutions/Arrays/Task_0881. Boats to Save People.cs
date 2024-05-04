1ce6b6df62e4ade4e8b13c8b3d53567f28925fdf
Nevadimka <morozovvadimka@gmail.com>
2024-05-04 20:56:58 +0300

    Added LeetCode Problem #0881

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0881. Boats to Save People.cs
@@ -0,0 +1,31 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0881
+    {
+        // #Array #Two Pointers #Greedy #Sorting
+
+        public int NumRescueBoats(int[] people, int limit)
+        {
+            Array.Sort(people);
+            int boatsCount = 0;
+            int left = 0, right = people.Length - 1;
+
+            while (left <= right)
+            {
+                if (people[left] + people[right] <= limit)
+                {
+                    left++;
+                    right--;
+                    boatsCount++;
+                }
+                else
+                {
+                    right--;
+                    boatsCount++;
+                }
+            }
+
+            return boatsCount;
+        }
+    }
+}
