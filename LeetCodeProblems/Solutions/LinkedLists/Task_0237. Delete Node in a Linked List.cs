21ccbd660c6accca4534ecf36d08f49b72d8da41
Nevadimka <morozovvadimka@gmail.com>
2024-05-05 19:45:10 +0300

    Added LeetCode Problem #0237

---
+++ LeetCodeProblems/Solutions/LinkedLists/Task_0237. Delete Node in a Linked List.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions.LinkedLists
+{
+    internal class Task_0237
+    {
+        // #Linked List
+
+        public void DeleteNode(ListNode node)
+        {
+            ListNode tmpNode = node;
+            while (node.next != null)
+            {
+                node.val = node.next.val;
+                (tmpNode, node) = (node, node.next);
+            }
+            tmpNode.next = null;
+
+            // or another one
+            // node.val = node.next.val;
+            // node.next = node.next.next;
+        }
+    }
+}
