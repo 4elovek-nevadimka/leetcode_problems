19a51d9e9668d803fafd98dc1668b5632354928b
Nevadimka <morozovvadimka@gmail.com>
2024-05-06 18:23:23 +0300

    Added LeetCode Problem #2487

---
+++ LeetCodeProblems/Solutions/LinkedLists/Task_2487. Remove Nodes From Linked List.cs
@@ -0,0 +1,29 @@
+﻿namespace Solutions.LinkedLists
+{
+    internal class Task_2487
+    {
+        // #Linked List #Stack #Recursion #Monotonic Stack
+
+        public ListNode RemoveNodes(ListNode head)
+        {
+            var stack = new Stack<int>();
+            while (head != null)
+            {
+                stack.Push(head.val);
+                head = head.next;
+            }
+
+            var newHead = new ListNode(stack.Pop());
+            while (stack.Count > 0)
+            {
+                var curNodeVal = stack.Pop();
+                if (curNodeVal >= newHead.val)
+                {
+                    newHead = new ListNode(curNodeVal, newHead);
+                }
+            }
+
+            return newHead;
+        }
+    }
+}
