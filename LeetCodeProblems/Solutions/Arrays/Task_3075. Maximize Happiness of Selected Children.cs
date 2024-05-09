25788f37c8f365a19af22cde014412a77b54aea7
Nevadimka <morozovvadimka@gmail.com>
2024-05-09 13:02:15 +0300

    Added LeetCode Problem #3075

---
+++ LeetCodeProblems/Solutions/Arrays/Task_3075. Maximize Happiness of Selected Children.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_3075
+    {
+        // #Array #Greedy #Sorting
+
+        public long MaximumHappinessSum(int[] happiness, int k)
+        {
+            long happinessSum = 0;
+            Array.Sort(happiness, (a, b) => b.CompareTo(a));
+            for (var i = 0; i < k; i++)
+            {
+                var val = happiness[i] - i;
+                if (val <= 0)
+                    break;
+                happinessSum += val;
+            }
+
+            return happinessSum;
+        }
+    }
+}
