590da16c858a58d551900aaf9bbba369b908103f
Nevadimka <morozovvadimka@gmail.com>
2024-05-16 14:13:19 +0300

    Added LeetCode Problem #2331

---
+++ LeetCodeProblems/Solutions/Trees/Task_2331. Evaluate Boolean Binary Tree.cs
@@ -0,0 +1,18 @@
+﻿namespace Solutions.Trees
+{
+    // #Tree #Depth-First Search #Binary Tree
+
+    internal class Task_2331
+    {
+        public bool EvaluateTree(TreeNode root)
+        {
+            if (root.val == 0) return false;
+            else if (root.val == 1) return true;
+
+            var left = EvaluateTree(root.left);
+            var right = EvaluateTree(root.right);
+
+            return root.val == 2 ? left || right : left && right;
+        }
+    }
+}
