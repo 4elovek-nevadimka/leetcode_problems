0aabee942fc31d6c8cd0828910bf865880ec6b26
Nevadimka <morozovvadimka@gmail.com>
2024-05-17 23:35:17 +0300

    Added LeetCode Problem #1325

---
+++ LeetCodeProblems/Solutions/Trees/Task_1325. Delete Leaves With a Given Value.cs
@@ -0,0 +1,20 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_1325
+    {
+        // #Tree #Depth-First Search #Binary Tree
+
+        public TreeNode RemoveLeafNodes(TreeNode root, int target)
+        {
+            if (root == null) return null;
+
+            root.left = RemoveLeafNodes(root.left, target);
+            root.right = RemoveLeafNodes(root.right, target);
+
+            if (root.left == null && root.right == null && root.val == target)
+                return null;
+
+            return root;
+        }
+    }
+}
