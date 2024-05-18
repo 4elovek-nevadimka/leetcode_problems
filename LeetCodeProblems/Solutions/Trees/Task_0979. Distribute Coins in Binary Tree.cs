6c7252d8745a7a334e9e47df69a40f19074a1e16
Nevadimka <morozovvadimka@gmail.com>
2024-05-18 22:32:03 +0300

    Added LeetCode Problem #0979

---
+++ LeetCodeProblems/Solutions/Trees/Task_0979. Distribute Coins in Binary Tree.cs
@@ -0,0 +1,28 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_0979
+    {
+        // #Tree #Depth-First Search #Binary Tree
+
+        private int _moves = 0;
+
+        public int DistributeCoins(TreeNode root)
+        {
+            Traverse(root);
+            return _moves;
+        }
+
+        private int Traverse(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+            var left = Traverse(node.left);
+            var right = Traverse(node.right);
+
+            _moves += Math.Abs(left) + Math.Abs(right);
+
+            return node.val + left + right - 1;
+        }
+    }
+}
