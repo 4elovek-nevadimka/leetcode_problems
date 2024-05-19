ed9c42b9fbbb1dbc7fa3830efd55ff48b984bccb
Nevadimka <morozovvadimka@gmail.com>
2024-05-19 16:48:49 +0300

    Added LeetCode Problem #3068

---
+++ LeetCodeProblems/Solutions/Trees/Task_3068. Find the Maximum Sum of Node Values.cs
@@ -0,0 +1,35 @@
+﻿namespace Solutions.Trees
+{
+    internal class Task_3068
+    {
+        // #Array #Dynamic Programming #Greedy #Bit Manipulation #Tree #Sorting
+
+        public long MaximumValueSum(int[] nums, int k, int[][] edges)
+        {
+            long result = 0;
+
+            foreach (long num in nums)
+                result += num;
+
+            var queue = new PriorityQueue<int, int>();
+
+            foreach (var num in nums)
+            {
+                var xor = num ^ k;
+                var diff = xor - num;
+
+                queue.Enqueue(diff, -diff);
+            }
+
+            while (queue.Count >= 2)
+            {
+                var value = queue.Dequeue() + queue.Dequeue();
+
+                if (value > 0)
+                    result += value;
+            }
+
+            return result;
+        }
+    }
+}
