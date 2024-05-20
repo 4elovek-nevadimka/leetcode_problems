bc96423ba5afc40d70e6474e9ea65fc5529885a8
Nevadimka <morozovvadimka@gmail.com>
2024-05-20 18:27:58 +0300

    Added LeetCode Problem #1863

---
+++ LeetCodeProblems/Solutions/BitManipulation/Task_1863. Sum of All Subset XOR Totals.cs
@@ -0,0 +1,21 @@
+﻿namespace Solutions.BitManipulation
+{
+    internal class Task_1863
+    {
+        // #Array #Math #Backtracking #Bit Manipulation #Combinatorics #Enumeration
+
+        public int SubsetXORSum(int[] nums)
+        {
+            return SubsetXORSumHelper(nums, 0, 0);
+        }
+
+        private int SubsetXORSumHelper(int[] nums, int index, int curXOR)
+        {
+            if (index == nums.Length)
+                return curXOR;
+
+            return SubsetXORSumHelper(nums, index + 1, curXOR) +
+                SubsetXORSumHelper(nums, index + 1, curXOR ^ nums[index]);
+        }
+    }
+}
