263ca9d7beec796a70a87679a379797c92de6099
Nevadimka <morozovvadimka@gmail.com>
2024-05-21 22:25:12 +0300

    Added LeetCode Problem #0078

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0078. Subsets.cs
@@ -0,0 +1,24 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0078
+    {
+        // #Array #Backtracking #Bit Manipulation
+
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var result = new List<IList<int>> { ([]) };
+
+            foreach (int num in nums)
+            {
+                var currentSubsetsCount = result.Count;
+                for (var i = 0; i < currentSubsetsCount; i++)
+                {
+                    var newSubset = new List<int>(result[i]) { num };
+                    result.Add(newSubset);
+                }
+            }
+
+            return result;
+        }
+    }
+}
