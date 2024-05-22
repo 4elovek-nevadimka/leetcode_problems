70aac3b3f1a92fcbf11b4eb01ea3e7035fd1b738
Nevadimka <morozovvadimka@gmail.com>
2024-05-22 12:00:18 +0300

    Added LeetCode Problem #0131

---
+++ LeetCodeProblems/Solutions/Strings/Task_0131. Palindrome Partitioning.cs
@@ -0,0 +1,46 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_0131
+    {
+        // #String #Dynamic Programming #Backtracking
+
+        public IList<IList<string>> Partition(string s)
+        {
+            var result = new List<IList<string>>();
+            var currentPartition = new List<string>();
+
+            PartitionHelper(s, 0, currentPartition, result);
+
+            return result;
+        }
+
+        private void PartitionHelper(string s, int start, IList<string> currentPartition, IList<IList<string>> result)
+        {
+            if (start == s.Length)
+            {
+                result.Add(new List<string>(currentPartition));
+                return;
+            }
+
+            for (var end = start; end < s.Length; end++)
+            {
+                if (IsPalindrome(s, start, end))
+                {
+                    currentPartition.Add(s.Substring(start, end - start + 1));
+                    PartitionHelper(s, end + 1, currentPartition, result);
+
+                    currentPartition.RemoveAt(currentPartition.Count - 1);
+                }
+            }
+        }
+
+        private bool IsPalindrome(string s, int left, int right)
+        {
+            while (left < right)
+                if (s[left++] != s[right--])
+                    return false;
+
+            return true;
+        }
+    }
+}
