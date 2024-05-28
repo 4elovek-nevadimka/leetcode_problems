4f30b4fafa580ec7b65de914438296f03902fcea
Nevadimka <morozovvadimka@gmail.com>
2024-05-28 17:56:44 +0300

    Added LeetCode Problem #1208

--- LeetCodeProblems/Solutions/Program.cs
+++ LeetCodeProblems/Solutions/Program.cs
@@ -15,6 +15,6 @@ using Solutions.Trees;
 Console.WriteLine("LeetCode Problems!");
 
 // run task here for debugging
-new Task_1608().Run();
+new Task_1208().Run();
 
 // Console.ReadKey();
---
+++ LeetCodeProblems/Solutions/Strings/Task_1208. Get Equal Substrings Within Budget.cs
@@ -0,0 +1,38 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_1208
+    {
+        // #String #Binary Search #Sliding Window #Prefix Sum
+
+        public void Run()
+        {
+            Console.WriteLine(EqualSubstring("abcd", "cdef", 1));
+        }
+
+        public int EqualSubstring(string s, string t, int maxCost)
+        {
+            int left = 0, right = 0;
+            int maxLength = 0, sumCost = 0;
+
+            while (right < s.Length)
+            {
+                var curCost = Math.Abs(s[right] - t[right]);
+                if (curCost + sumCost <= maxCost)
+                {
+                    right++;
+                    sumCost += curCost;
+                    maxLength = Math.Max(maxLength, right - left);
+                }
+                else
+                {
+                    if (sumCost > 0)
+                        sumCost -= Math.Abs(s[left] - t[left]);
+                    left++;
+                    if (left > right)
+                        right++;
+                }
+            }
+            return maxLength;
+        }
+    }
+}
