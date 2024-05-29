43872eb9a6a388b124237a0d6e1a7a79cb677079
Nevadimka <morozovvadimka@gmail.com>
2024-05-29 17:21:47 +0300

    Added LeetCode Problem #1404

---
+++ LeetCodeProblems/Solutions/BitManipulation/Task_1404. Number of Steps to Reduce a Number in Binary Representation to One.cs
@@ -0,0 +1,27 @@
+﻿namespace Solutions.BitManipulation
+{
+    internal class Task_1404
+    {
+        // #String #Bit Manipulation
+
+        public int NumSteps(string s)
+        {
+            int steps = 0, carry = 0;
+            for (int i = s.Length - 1; i > 0; i--)
+            {
+                if (s[i] - '0' + carry == 1)
+                {
+                    carry = 1;
+                    // one step for subtracting 1 and one step for division
+                    steps += 2; 
+                }
+                else
+                {
+                    // only one step for division
+                    steps += 1;
+                }
+            }
+            return steps + carry;
+        }
+    }
+}
