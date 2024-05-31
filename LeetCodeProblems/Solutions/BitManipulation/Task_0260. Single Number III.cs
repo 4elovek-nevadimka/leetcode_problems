0e70b36ef3ca669290005ef5a7e6cb05b3c91270
Nevadimka <morozovvadimka@gmail.com>
2024-05-31 19:52:52 +0300

    Added LeetCode Problem #0260

---
+++ LeetCodeProblems/Solutions/BitManipulation/Task_0260. Single Number III.cs
@@ -0,0 +1,27 @@
+﻿namespace Solutions.BitManipulation
+{
+    internal class Task_0260
+    {
+        // #Array #Bit Manipulation
+
+        public int[] SingleNumber(int[] nums)
+        {
+            var xorAll = 0;
+            foreach (int num in nums)
+                xorAll ^= num;
+
+            xorAll &= -xorAll;
+
+            var result = new int[2];
+            foreach (int num in nums)
+            {
+                if ((num & xorAll) == 0)
+                    result[0] ^= num;
+                else
+                    result[1] ^= num;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeProblems/Solutions/BitManipulation/Task_1442. Count Triplets That Can Form Two Arrays of Equal XOR.cs
@@ -0,0 +1,28 @@
+﻿namespace Solutions.BitManipulation
+{
+    internal class Task_1442
+    {
+        // #Array #Hash Table #Math #Bit Manipulation #Prefix Sum
+
+        public int CountTriplets(int[] arr)
+        {
+            var n = arr.Length;
+            var prefixXor = new int[n + 1];
+
+            for (var i = 0; i < n; i++)
+                prefixXor[i + 1] = prefixXor[i] ^ arr[i];
+
+            var count = 0;
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = i + 1; j < n; j++)
+                {
+                    if (prefixXor[i] == prefixXor[j + 1])
+                        count += (j - i);
+                }
+            }
+
+            return count;
+        }
+    }
+}
