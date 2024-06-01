b7761cec0827955590432175f70922c6eecc4e0c
Nevadimka <morozovvadimka@gmail.com>
2024-06-01 21:02:54 +0300

    Added LeetCode Problem #3110

---
+++ LeetCodeProblems/Solutions/Strings/Task_3110. Score of a String.cs
@@ -0,0 +1,16 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_3110
+    {
+        // #String
+
+        public int ScoreOfString(string s)
+        {
+            var score = 0;
+            for (var i = 1; i < s.Length; i++)
+                score += Math.Abs(s[i - 1] - s[i]);
+
+            return score;
+        }
+    }
+}
