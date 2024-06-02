e35518b215f4977f97fca6e270e3bc9045d90222
Nevadimka <morozovvadimka@gmail.com>
2024-06-02 11:13:50 +0300

    Added LeetCode Problem #0344

---
+++ LeetCodeProblems/Solutions/Strings/Task_0344. Reverse String.cs
@@ -0,0 +1,14 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_0344
+    {
+        // #String #Two Pointers
+
+        public void ReverseString(char[] s)
+        {
+            int left = 0, right = s.Length - 1;
+            while (left < right)
+                (s[left], s[right]) = (s[right--], s[left++]);
+        }
+    }
+}
