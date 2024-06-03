44a3ca95bb020acd56065f070fcb916110c78698
Nevadimka <morozovvadimka@gmail.com>
2024-06-03 21:19:05 +0300

    Added LeetCode Problem #2486

---
+++ LeetCodeProblems/Solutions/Strings/Task_2486. Append Characters to String to Make Subsequence.cs
@@ -0,0 +1,20 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_2486
+    {
+        // #Two Pointers #String #Greedy
+
+        public int AppendCharacters(string s, string t)
+        {
+            int left = 0, right = 0;
+            while (!(left == s.Length || right == t.Length))
+            {
+                if (s[left] == t[right])
+                    right++;
+                left++;
+            }
+
+            return t.Length - right;
+        }
+    }
+}
