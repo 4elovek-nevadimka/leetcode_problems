84f2638dca697b94daeb7ec8e51c5f7162987c12
Nevadimka <morozovvadimka@gmail.com>
2024-06-04 20:09:31 +0300

    Added LeetCode Problem #0409

---
+++ LeetCodeProblems/Solutions/Strings/Task_0409. Longest Palindrome.cs
@@ -0,0 +1,36 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_0409
+    {
+        // #Hash Table #String #Greedy
+
+        public int LongestPalindrome(string s)
+        {
+            var dic = new Dictionary<char, int>();
+            foreach (var c in s)
+            {
+                if (!dic.ContainsKey(c))
+                    dic[c] = 0;
+                dic[c]++;
+            }
+
+            var longest = 0;
+            var addOne = false;
+            foreach (var key in dic.Keys)
+            {
+
+                if (int.IsEvenInteger(dic[key]))
+                    longest += dic[key];
+                else
+                {
+                    if (!addOne) addOne = true;
+                    longest += dic[key] - 1;
+                }
+            }
+            if (addOne)
+                longest++;
+
+            return longest;
+        }
+    }
+}
