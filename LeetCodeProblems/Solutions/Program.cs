99926aae66e8b3741d61ff13fc747f9ae392677a
Nevadimka <morozovvadimka@gmail.com>
2024-06-05 12:37:45 +0300

    Added LeetCode Problem #1002

--- LeetCodeProblems/Solutions/Program.cs
+++ LeetCodeProblems/Solutions/Program.cs
@@ -15,6 +15,6 @@ using Solutions.Trees;
 Console.WriteLine("LeetCode Problems!");
 
 // run task here for debugging
-new Task_1208().Run();
+new Task_1002().Run();
 
 // Console.ReadKey();
---
+++ LeetCodeProblems/Solutions/Strings/Task_1002. Find Common Characters.cs
@@ -0,0 +1,45 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_1002
+    {
+        // #
+
+        public void Run()
+        {
+            var result = CommonChars(["bella", "label", "roller"]);
+            Array.ForEach(result.ToArray(), Console.Write);
+        }
+
+        public IList<string> CommonChars(string[] words)
+        {
+            var shortestWord = words[0];
+            foreach (var word in words)
+                if (shortestWord.Length > word.Length)
+                    shortestWord = word;
+
+            var letters = new Dictionary<char, int>();
+            foreach (var c in shortestWord)
+                if (letters.TryGetValue(c, out int value))
+                    letters[c] = ++value;
+                else
+                    letters[c] = 1;
+
+            foreach (var word in words)
+            {
+                foreach (var letter in letters.Keys)
+                {
+                    var letterInWord = word.AsSpan().Count(letter);
+                    if (letterInWord < letters[letter])
+                        letters[letter] = letterInWord;
+                }
+            }
+
+            var result = new List<string>();
+            foreach (var letter in letters.Keys)
+                for (var i = 0; i < letters[letter]; i++)
+                    result.Add(letter.ToString());
+
+            return result;
+        }
+    }
+}
