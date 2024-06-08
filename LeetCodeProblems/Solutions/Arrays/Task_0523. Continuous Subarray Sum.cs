f2ff620209e7cfd66ae58f872ff28cf8ebe45892
Nevadimka <morozovvadimka@gmail.com>
2024-06-08 22:47:45 +0300

    Added LeetCode Problem #0523

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0523. Continuous Subarray Sum.cs
@@ -0,0 +1,34 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0523
+    {
+        // #Array #Hash Table #Math #Prefix Sum
+
+        public bool CheckSubarraySum(int[] nums, int k)
+        {
+            var remainderMap = new Dictionary<int, int> { [0] = -1 };
+            var cumulativeSum = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                cumulativeSum += nums[i];
+                var remainder = cumulativeSum % k;
+
+                if (remainder < 0)
+                    remainder += k;
+
+                if (remainderMap.ContainsKey(remainder))
+                {
+                    if (i - remainderMap[remainder] > 1)
+                        return true;
+                }
+                else
+                {
+                    remainderMap[remainder] = i;
+                }
+            }
+
+            return false;
+        }
+    }
+}
