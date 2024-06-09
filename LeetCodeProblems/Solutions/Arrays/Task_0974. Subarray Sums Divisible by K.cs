5dba18044714e9e3d5a16cefec83629716ae0bcb
Nevadimka <morozovvadimka@gmail.com>
2024-06-09 22:45:35 +0300

    Added LeetCode Problem #0974

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0974. Subarray Sums Divisible by K.cs
@@ -0,0 +1,34 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0974
+    {
+        // #Array #Hash Table #Prefix Sum
+
+        public int SubarraysDivByK(int[] nums, int k)
+        {
+            var remainderMap = new Dictionary<int, int> { [0] = -1 };
+            int cumulativeSum = 0, count = 0;
+
+            foreach (var num in nums)
+            {
+                cumulativeSum += num;
+                var remainder = cumulativeSum % k;
+
+                if (remainder < 0)
+                    remainder += k;
+
+                if (remainderMap.ContainsKey(remainder))
+                {
+                    count += remainderMap[remainder];
+                    remainderMap[remainder]++;
+                }
+                else
+                {
+                    remainderMap[remainder] = 1;
+                }
+            }
+
+            return count;
+        }
+    }
+}
