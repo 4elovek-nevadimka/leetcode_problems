184cbc9cb4214fade731b37fc5c73c2ce74baf03
Nevadimka <morozovvadimka@gmail.com>
2024-06-10 21:53:50 +0300

    Added LeetCode Problem #1051

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1051. Height Checker.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1051
+    {
+        // #Array #Sorting #Counting Sort
+
+        public int HeightChecker(int[] heights)
+        {
+            var sortedArr = new int[heights.Length];
+            heights.CopyTo(sortedArr, 0);
+
+            Array.Sort(sortedArr);
+            var diffCounter = 0;
+
+            for (var i = 0; i < heights.Length; i++)
+                if (heights[i] != sortedArr[i])
+                    diffCounter++;
+
+            return diffCounter;
+        }
+    }
+}
