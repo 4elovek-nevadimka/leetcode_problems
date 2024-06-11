e9d94674bf8d8f77cf596a9579228e55afd0389c
Nevadimka <morozovvadimka@gmail.com>
2024-06-11 15:25:15 +0300

    Added LeetCode Problem #1122

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1122. Relative Sort Array.cs
@@ -0,0 +1,30 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1122
+    {
+        // #Array #Hash Table #Sorting #Counting Sort
+
+        public int[] RelativeSortArray(int[] arr1, int[] arr2)
+        {
+            var maxValue = 0;
+            foreach (var val in arr1)
+                if (val > maxValue)
+                    maxValue = val;
+
+            var helpArr = new int[maxValue + 1];
+            foreach (var val in arr1)
+                helpArr[val]++;
+
+            var index = 0;
+            foreach (var val in arr2)
+                while (helpArr[val]-- > 0)
+                    arr1[index++] = val;
+
+            for (var i = 0; i < helpArr.Length; i++)
+                while (helpArr[i]-- > 0)
+                    arr1[index++] = i;
+
+            return arr1;
+        }
+    }
+}
