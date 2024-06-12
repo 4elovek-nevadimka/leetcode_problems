98c27fc26e41adf7396e32ae9be04d5e8af1e650
Nevadimka <morozovvadimka@gmail.com>
2024-06-12 11:44:28 +0300

    Added LeetCode Problem #0075

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0075. Sort Colors.cs
@@ -0,0 +1,27 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0075
+    {
+        // #Array #Two Pointers #Sorting
+
+        public void SortColors(int[] nums)
+        {
+            int zeros = 0, ones = 0;
+            foreach (var num in nums)
+                if (num == 0)
+                    zeros++;
+                else if (num == 1)
+                    ones++;
+
+            int index = 0;
+            for (var i = 0; i < zeros; i++)
+                nums[index++] = 0;
+
+            for (var i = 0; i < ones; i++)
+                nums[index++] = 1;
+
+            while (index < nums.Length)
+                nums[index++] = 2;
+        }
+    }
+}
