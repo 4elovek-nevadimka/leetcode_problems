b46e9cc5da422ee91b8baa993f4fd47b36fdc52a
Nevadimka <morozovvadimka@gmail.com>
2024-06-13 20:39:30 +0300

    Added LeetCode Problem #2037

---
+++ LeetCodeProblems/Solutions/Arrays/Task_2037. Minimum Number of Moves to Seat Everyone.cs
@@ -0,0 +1,19 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_2037
+    {
+        // #Array #Greedy #Sorting
+
+        public int MinMovesToSeat(int[] seats, int[] students)
+        {
+            Array.Sort(seats);
+            Array.Sort(students);
+
+            var moves = 0;
+            for (var i = 0; i < seats.Length; i++)
+                moves += Math.Abs(seats[i] - students[i]);
+
+            return moves;
+        }
+    }
+}
