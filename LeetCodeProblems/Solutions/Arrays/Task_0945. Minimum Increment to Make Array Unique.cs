5750c4d63f81a18f3ba42f5af42c503c20931f8d
Nevadimka <morozovvadimka@gmail.com>
2024-06-14 21:44:08 +0300

    Added LeetCode Problem #0945

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0945. Minimum Increment to Make Array Unique.cs
@@ -0,0 +1,24 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0945
+    {
+        // #Array #Greedy #Sorting #Counting
+
+        public int MinIncrementForUnique(int[] nums)
+        {
+            Array.Sort(nums);
+            var count = 0;
+            for (var i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] <= nums[i - 1])
+                {
+                    var diff = nums[i - 1] - nums[i] + 1;
+                    nums[i] += diff;
+                    count += diff;
+                }
+            }
+
+            return count;
+        }
+    }
+}
