a34d3233d55333c939f20ba5982025ac10dcd6a4
Nevadimka <morozovvadimka@gmail.com>
2024-06-17 20:26:59 +0300

    Added LeetCode Problem #0633

---
+++ LeetCodeProblems/Solutions/Mathematics/Task_0633. Sum of Square Numbers.cs
@@ -0,0 +1,23 @@
+﻿namespace Solutions.Mathematics
+{
+    internal class Task_0633
+    {
+        // #Math #Two Pointers #Binary Search
+
+        public bool JudgeSquareSum(int c)
+        {
+            int a = 0, b = (int)Math.Sqrt(c);
+            while (a <= b)
+            {
+                long sum = (long)(a * a) + (long)(b * b);
+                if (sum == c)
+                    return true;
+                else if (sum > c)
+                    b--;
+                else
+                    a++;
+            }
+            return false;
+        }
+    }
+}
