d55117d30cb53e307a179d4fb95405f13c022537
Nevadimka <morozovvadimka@gmail.com>
2024-06-21 23:47:58 +0300

    temp commit

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1052. Grumpy Bookstore Owner.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1052
+    {
+        // #Array #Sliding Window
+
+        public int MaxSatisfied(int[] customers, int[] grumpy, int minutes)
+        {
+            int n = customers.Length;
+            int currentSatisfaction = 0;
+            int grumyIndex = -1;
+            int maxSatisfaction = 0;
+
+            for (var i = 0; i < n - minutes; i++)
+            {
+
+            }
+
+            return maxSatisfaction;
+        }
+    }
+}
---
+++ LeetCodeProblems/Solutions/Arrays/Task_1552. Magnetic Force Between Two Balls.cs
@@ -0,0 +1,43 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1552
+    {
+        // #Array #Binary Search #Sorting
+
+        public int MaxDistance(int[] position, int m)
+        {
+            Array.Sort(position);
+
+            int left = 1, right = position[^1] - position[0];
+            while (left < right)
+            {
+                int mid = left + (right - left + 1) / 2;
+                if (CanPlaceBalls(position, m, mid))
+                    left = mid;
+                else
+                    right = mid - 1;
+            }
+
+            return left;
+        }
+
+        private bool CanPlaceBalls(int[] position, int m, int minDist)
+        {
+            int count = 1;
+            int lastPosition = position[0];
+
+            for (int i = 1; i < position.Length; i++)
+            {
+                if (position[i] - lastPosition >= minDist)
+                {
+                    count++;
+                    lastPosition = position[i];
+                    if (count == m)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
