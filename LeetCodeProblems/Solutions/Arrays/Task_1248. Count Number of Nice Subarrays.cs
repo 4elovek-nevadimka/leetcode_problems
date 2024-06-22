9d5deff20d0f8c2d27e5de955f18874937e5d428
Nevadimka <morozovvadimka@gmail.com>
2024-06-22 16:24:37 +0300

    Added LeetCode Problem #1248

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1248. Count Number of Nice Subarrays.cs
@@ -0,0 +1,29 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1248
+    {
+        // #Array #Hash Table #Math #Sliding Window
+
+        public int NumberOfSubarrays(int[] nums, int k)
+        {
+            var frequencyDic = new Dictionary<int, int> { [0] = 1 };
+
+            int prefixSum = 0, result = 0;
+            foreach (var num in nums)
+            {
+                if (num % 2 == 1)
+                    prefixSum++;
+
+                if (frequencyDic.ContainsKey(prefixSum - k))
+                    result += frequencyDic[prefixSum - k];
+
+                if (frequencyDic.TryGetValue(prefixSum, out int value))
+                    frequencyDic[prefixSum] = ++value;
+                else
+                    frequencyDic[prefixSum] = 1;
+            }
+
+            return result;
+        }
+    }
+}
