2aff85a11986e997fc8b308d614a5ff0f7aa65fd
Nevadimka <morozovvadimka@gmail.com>
2024-06-23 18:49:16 +0300

    Added LeetCode Problem #1438

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cs
@@ -0,0 +1,45 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1438
+    {
+        // #Array #Queue #Sliding Window #Heap(Priority Queue) #Ordered Set #Monotonic Queue
+
+        public int LongestSubarray(int[] nums, int limit)
+        {
+            int left = 0, result = 0;
+            LinkedList<int> maxDeque = new (), minDeque = new();
+
+            for (int right = 0; right < nums.Length; right++)
+            {
+                while (maxDeque.Count > 0 && nums[maxDeque.Last.Value] <= nums[right])
+                {
+                    maxDeque.RemoveLast();
+                }
+                while (minDeque.Count > 0 && nums[minDeque.Last.Value] >= nums[right])
+                {
+                    minDeque.RemoveLast();
+                }
+
+                maxDeque.AddLast(right);
+                minDeque.AddLast(right);
+
+                while (nums[maxDeque.First.Value] - nums[minDeque.First.Value] > limit)
+                {
+                    if (maxDeque.First.Value == left)
+                    {
+                        maxDeque.RemoveFirst();
+                    }
+                    if (minDeque.First.Value == left)
+                    {
+                        minDeque.RemoveFirst();
+                    }
+                    left++;
+                }
+
+                result = Math.Max(result, right - left + 1);
+            }
+
+            return result;
+        }
+    }
+}
