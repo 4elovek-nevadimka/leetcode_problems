59bafe2d7776b300110d521b5eb8c7f81bc4cfd1
Nevadimka <morozovvadimka@gmail.com>
2024-06-24 17:16:22 +0300

    Added LeetCode Problem #0995

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0995. Minimum Number of K Consecutive Bit Flips.cs
@@ -0,0 +1,29 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0995
+    {
+        // #Array #Bit Manipulation #Queue #Sliding Window #Prefix Sum
+
+        public int MinKBitFlips(int[] nums, int k)
+        {
+            int currentFlips = 0, totalFlips = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i >= k && nums[i - k] == 2)
+                    currentFlips--;
+
+                if ((currentFlips % 2) == nums[i])
+                {
+                    if (i + k > nums.Length)
+                        return -1;
+
+                    nums[i] = 2;
+                    currentFlips++;
+                    totalFlips++;
+                }
+            }
+
+            return totalFlips;
+        }
+    }
+}
