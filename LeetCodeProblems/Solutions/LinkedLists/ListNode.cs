6e18a0fff1c83730c050a7eb3834657a50e9b2fc
Nevadimka <morozovvadimka@gmail.com>
2024-06-25 20:21:44 +0300

    updated ListNode and ListNodeGenerator

--- LeetCodeProblems/Solutions/LinkedLists/ListNode.cs
+++ LeetCodeProblems/Solutions/LinkedLists/ListNode.cs
@@ -12,23 +12,4 @@
             this.next = next;
         }
     }
-
-    internal static class ListNodeGenerator
-    {
-        internal static ListNode Generate(int[] nodesValues)
-        {
-            if (nodesValues == null || nodesValues.Length == 0)
-                return null;
-
-            ListNode empty = new(), current = empty;
-            foreach (int nodeValue in nodesValues)
-            {
-                current.next = new ListNode(nodeValue);
-                current = current.next;
-            }
-
-            return empty.next;
-        }
-    }
- 
 }
---
+++ LeetCodeProblems/Solutions/LinkedLists/ListNodeGenerator.cs
@@ -0,0 +1,20 @@
+﻿namespace Solutions.LinkedLists
+{
+    internal static class ListNodeGenerator
+    {
+        internal static ListNode Generate(int[] nodesValues)
+        {
+            if (nodesValues == null || nodesValues.Length == 0)
+                return null;
+
+            ListNode empty = new(), current = empty;
+            foreach (int nodeValue in nodesValues)
+            {
+                current.next = new ListNode(nodeValue);
+                current = current.next;
+            }
+
+            return empty.next;
+        }
+    }
+}
