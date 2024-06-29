db274582dc1a987de81635be5080b725ee18146c
Nevadimka <morozovvadimka@gmail.com>
2024-06-29 19:13:41 +0300

    One more solution for #2192

--- LeetCodeProblems/Solutions/Graphs/Task_2192. All Ancestors of a Node in a Directed Acyclic Graph.cs
+++ LeetCodeProblems/Solutions/Graphs/Task_2192. All Ancestors of a Node in a Directed Acyclic Graph.cs
@@ -12,6 +12,11 @@
 
         public IList<IList<int>> GetAncestors(int n, int[][] edges)
         {
+            return Solution2(n, edges);
+        }
+
+        public IList<IList<int>> Solution1(int n, int[][] edges)
+        {
             var ancestorsArr = new HashSet<int>[n];
             for (var i = 0; i < n; i++)
                 ancestorsArr[i] = [];
@@ -54,6 +59,68 @@
                 result.Add([.. ancestorsArr[i].OrderBy(x => x)]);
 
             return result;
+        }
+
+        public IList<IList<int>> Solution2(int n, int[][] edges)
+        {
+            var adjacencyList = new List<int>[n];
+            for (var i = 0; i < n; i++)
+                adjacencyList[i] = [];
+
+            var indegree = new int[n];
+            foreach (var edge in edges)
+            {
+                adjacencyList[edge[0]].Add(edge[1]);
+                indegree[edge[1]]++;
+            }
+
+            var nodesWithZeroIndegree = new Queue<int>();
+            for (var i = 0; i < indegree.Length; i++)
+            {
+                if (indegree[i] == 0)
+                    nodesWithZeroIndegree.Enqueue(i);
+            }
+
+            var topologicalOrder = new List<int>();
+            while (nodesWithZeroIndegree.Count > 0)
+            {
+                int currentNode = nodesWithZeroIndegree.Dequeue();
+                topologicalOrder.Add(currentNode);
+
+                foreach (int neighbor in adjacencyList[currentNode])
+                {
+                    indegree[neighbor]--;
+                    if (indegree[neighbor] == 0)
+                    {
+                        nodesWithZeroIndegree.Enqueue(neighbor);
+                    }
+                }
+            }
+
+            var ancestorsList = new List<IList<int>>();
+            var ancestorsSetList = new List<HashSet<int>>();
+            for (var i = 0; i < n; i++)
+            {
+                ancestorsList.Add([]);
+                ancestorsSetList.Add([]);
+            }
+
+            foreach (var node in topologicalOrder)
+            {
+                foreach (var neighbor in adjacencyList[node])
+                {
+                    ancestorsSetList[neighbor].Add(node);
+                    ancestorsSetList[neighbor].UnionWith(ancestorsSetList[node]);
+                }
+            }
+
+            for (int i = 0; i < ancestorsList.Count; i++)
+            {
+                ((List<int>)ancestorsList[i]).AddRange(ancestorsSetList[i]);
+                ((List<int>)ancestorsList[i]).Sort();
+            }
+
+            return ancestorsList;
         }
     }
 }
