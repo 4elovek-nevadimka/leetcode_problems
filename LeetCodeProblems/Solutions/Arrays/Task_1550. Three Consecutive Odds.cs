d8beae555ee41770c01f527a3dc5a1763f1a11fc
Nevadimka <morozovvadimka@gmail.com>
2024-07-01 16:02:06 +0300

    Added LeetCode Problem #1550

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1550. Three Consecutive Odds.cs
@@ -0,0 +1,23 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1550
+    {
+        // #Array
+
+        public bool ThreeConsecutiveOdds(int[] arr)
+        {
+            if (arr.Length < 3)
+                return false;
+
+            for (var i = 0; i < arr.Length - 2; i++)
+            {
+                if (int.IsOddInteger(arr[i]) 
+                    && int.IsOddInteger(arr[i + 1]) 
+                    && int.IsOddInteger(arr[i + 2]))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
