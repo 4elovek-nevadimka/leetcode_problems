c08284ebc2d82b094c052b2a247eddc6ea611263
Nevadimka <morozovvadimka@gmail.com>
2024-07-02 13:12:16 +0300

    Added LeetCode Problem #0350

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0350. Intersection of Two Arrays II.cs
@@ -0,0 +1,27 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0350
+    {
+        // #Array #Hash Table #Two Pointers #Binary Search #Sorting
+
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            var freqDic = new Dictionary<int, int>();
+            foreach (var num in nums1)
+                if (freqDic.TryGetValue(num, out int value))
+                    freqDic[num] = ++value;
+                else
+                    freqDic[num] = 1;
+
+            var result = new List<int>(freqDic.Count);
+            foreach (var num in nums2)
+                if (freqDic.TryGetValue(num, out int value) && value > 0)
+                {
+                    result.Add(num);
+                    freqDic[num] = --value;
+                }
+
+            return [.. result];
+        }
+    }
+}
