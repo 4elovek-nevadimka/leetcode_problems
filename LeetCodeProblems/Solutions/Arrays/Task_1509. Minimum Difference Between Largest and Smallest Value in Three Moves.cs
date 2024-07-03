f3a0d8256742d94ea35fb0272bba603ab7d79527
Nevadimka <morozovvadimka@gmail.com>
2024-07-03 23:21:59 +0300

    Added LeetCode Problem #1509

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1509. Minimum Difference Between Largest and Smallest Value in Three Moves.cs
@@ -0,0 +1,21 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1509
+    {
+        // #Array #Greedy #Sorting
+        public int MinDifference(int[] nums)
+        {
+            if (nums.Length <= 4)
+                return 0;
+
+            Array.Sort(nums);
+
+            var min1 = 
+                Math.Min(nums[^1] - nums[3], nums[^2] - nums[2]);
+            var min2 = 
+                Math.Min(nums[^3] - nums[1], nums[^4] - nums[0]);
+
+            return Math.Min(min1, min2);
+        }
+    }
+}
