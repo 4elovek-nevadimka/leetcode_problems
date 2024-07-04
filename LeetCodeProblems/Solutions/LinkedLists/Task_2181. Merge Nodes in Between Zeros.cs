54dd558780ea5c31a625a25838596c7c55310bbf
Nevadimka <morozovvadimka@gmail.com>
2024-07-04 22:52:33 +0300

    Added LeetCode Problem #2181

---
+++ LeetCodeProblems/Solutions/LinkedLists/Task_2181. Merge Nodes in Between Zeros.cs
@@ -0,0 +1,42 @@
+﻿namespace Solutions.LinkedLists
+{
+    internal class Task_2181
+    {
+        // #Linked List #Simulation
+
+        public ListNode MergeNodes(ListNode head)
+        {
+            ListNode newHead = null, newNode = null, previousNode = null;
+            int sum = 0;
+
+            while (head != null)
+            {
+                if (head.val == 0)
+                {
+                    if (sum > 0)
+                    {
+                        newNode = new ListNode(sum);
+                        if (newHead == null)
+                        {
+                            newHead = newNode;
+                            previousNode = newNode;
+                        }
+                        else
+                        {
+                            previousNode.next = newNode;
+                            previousNode = newNode;
+                        }
+                        sum = 0;
+                    }
+                }
+                else
+                {
+                    sum += head.val;
+                }
+                head = head.next;
+            }
+
+            return newHead;
+        }
+    }
+}
