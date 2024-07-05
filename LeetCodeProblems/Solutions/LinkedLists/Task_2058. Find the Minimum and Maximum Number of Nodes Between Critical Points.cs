bd68a23fd49cccfd5e042fbead1108156afb25c1
Nevadimka <morozovvadimka@gmail.com>
2024-07-05 22:06:28 +0300

    Added LeetCode Problem #2058

---
+++ LeetCodeProblems/Solutions/LinkedLists/Task_2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.cs
@@ -0,0 +1,40 @@
+﻿namespace Solutions.LinkedLists
+{
+    internal class Task_2058
+    {
+        // #Linked List
+
+        public int[] NodesBetweenCriticalPoints(ListNode head)
+        {
+            int index = 1, first = 0, previous = 0, min = int.MaxValue;
+            ListNode previousNode = head;
+            head = head.next;
+
+            while (head != null)
+            {
+                if (head.next == null)
+                    break;
+
+                if ((head.val > previousNode.val && head.val > head.next.val) 
+                    || (head.val < previousNode.val && head.val < head.next.val))
+                {
+                    if (first == 0)
+                        first = previous = index;
+                    else
+                    {
+                        min = Math.Min(min, index - previous);
+                        previous = index;
+                    }
+                }
+                previousNode = head;
+                head = head.next;
+                index++;
+            }
+
+            if (min == int.MaxValue)
+                return [-1, -1];
+
+            return [min, previous - first];
+        }
+    }
+}
