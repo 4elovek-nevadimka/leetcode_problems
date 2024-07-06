64cfb8bd03ed24589f94f833c0574a5e936aeac0
Nevadimka <morozovvadimka@gmail.com>
2024-07-06 23:05:23 +0300

    Added LeetCode Problem #2582

---
+++ LeetCodeProblems/Solutions/Mathematics/Task_2582. Pass the Pillow.cs
@@ -0,0 +1,23 @@
+﻿namespace Solutions.Mathematics
+{
+    internal class Task_2582
+    {
+        // #Math #Simulation
+
+        public int PassThePillow(int n, int time)
+        {
+            if (time < n)
+                return time + 1;
+
+            if (time == n)
+                return n - 1;
+
+            var div = time / (n - 1);
+            var rest = time % (n - 1);
+            if (div % 2 == 0)
+                return rest + 1;
+            else
+                return n - rest;
+        }
+    }
+}
