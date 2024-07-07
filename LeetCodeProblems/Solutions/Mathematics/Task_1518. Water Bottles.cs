4063eef8333c635c8f6c94065ad88c9556816591
Nevadimka <morozovvadimka@gmail.com>
2024-07-07 22:46:39 +0300

    Added LeetCode Problem #1518

---
+++ LeetCodeProblems/Solutions/Mathematics/Task_1518. Water Bottles.cs
@@ -0,0 +1,23 @@
+﻿namespace Solutions.Mathematics
+{
+    internal class Task_1518
+    {
+        // #Math #Simulation
+
+        public int NumWaterBottles(int numBottles, int numExchange)
+        {
+            int waterBottles = 0;
+            while (numBottles >= numExchange)
+            {
+                int divBottles = numBottles / numExchange;
+
+                waterBottles += numExchange * divBottles;
+                numBottles -= numExchange * divBottles;
+
+                numBottles += divBottles;
+            }
+
+            return waterBottles + numBottles;
+        }
+    }
+}
