1e2f359f956a0567493f56a6b593c349edeae433
Nevadimka <morozovvadimka@gmail.com>
2024-07-08 20:36:27 +0300

    Added LeetCode Problem #1823

---
+++ LeetCodeProblems/Solutions/Mathematics/Task_1823. Find the Winner of the Circular Game.cs
@@ -0,0 +1,24 @@
+﻿namespace Solutions.Mathematics
+{
+    internal class Task_1823
+    {
+        // #Array #Math #Recursion #Queue #Simulation
+
+        public int FindTheWinner(int n, int k)
+        {
+            var queue = new Queue<int>(n);
+            for (var i = 1; i <= n; i++)
+                queue.Enqueue(i);
+
+            while (queue.Count > 1)
+            {
+                for (var i = 0; i < k - 1; i++)
+                    queue.Enqueue(queue.Dequeue());
+
+                queue.Dequeue();
+            }
+
+            return queue.Dequeue();
+        }
+    }
+}
