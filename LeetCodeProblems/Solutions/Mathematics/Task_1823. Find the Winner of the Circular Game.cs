2c60d97a541e4c55bf058d1eedf510ae17427b59
Nevadimka <morozovvadimka@gmail.com>
2024-07-08 20:49:15 +0300

    One more solution for #1823

--- LeetCodeProblems/Solutions/Mathematics/Task_1823. Find the Winner of the Circular Game.cs
+++ LeetCodeProblems/Solutions/Mathematics/Task_1823. Find the Winner of the Circular Game.cs
@@ -6,6 +6,11 @@
 
         public int FindTheWinner(int n, int k)
         {
+            return Solution1(n, k);
+        }
+
+        private int Solution1(int n, int k)
+        {
             var queue = new Queue<int>(n);
             for (var i = 1; i <= n; i++)
                 queue.Enqueue(i);
@@ -19,6 +24,31 @@
             }
 
             return queue.Dequeue();
+        }
+
+        private int Solution2(int n, int k)
+        {
+            var list = new List<int>(n);
+            for (var i = 1; i <= n; i++)
+                list.Add(i);
+
+            int index = 0;
+            while (list.Count > 1)
+            {
+                index = (index + k - 1) % list.Count;
+                list.RemoveAt(index);
+            }
+
+            return list[0];
+        }
+
+        private int Solution3(int n, int k)
+        {
+            int winner = 0;
+            for (int i = 2; i <= n; i++)
+                winner = (winner + k) % i;
+
+            return winner + 1;
         }
     }
 }
