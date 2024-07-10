29cb8a7a4f8f7d473ab9e2c68b0a44c2f94f4454
Nevadimka <morozovvadimka@gmail.com>
2024-07-10 14:14:06 +0300

    Added LeetCode Problem #1598

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1598. Crawler Log Folder.cs
@@ -0,0 +1,27 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1598
+    {
+        // #Array #String #Stack
+
+        public int MinOperations(string[] logs)
+        {
+            int level = 0;
+            foreach (var log in logs)
+            {
+                if (log == "./")
+                    continue;
+
+                if (log == "../")
+                {
+                    if (level > 0)
+                        level--;
+                }
+                else
+                    level++;
+            }
+
+            return level;
+        }
+    }
+}
