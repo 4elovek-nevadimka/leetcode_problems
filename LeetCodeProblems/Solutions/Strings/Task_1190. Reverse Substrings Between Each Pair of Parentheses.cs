cad037768ad7f237c376fca32cacf8d518393243
Nevadimka <morozovvadimka@gmail.com>
2024-07-11 23:05:44 +0300

    Added LeetCode Problem #1190

---
+++ LeetCodeProblems/Solutions/Strings/Task_1190. Reverse Substrings Between Each Pair of Parentheses.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace Solutions.Strings
+{
+    internal class Task_1190
+    {
+        // #String #Stack
+
+        public string ReverseParentheses(string s)
+        {
+            var parenthesesIndices = new Stack<int>();
+            var result = new StringBuilder();
+
+            foreach (var c in s)
+            {
+                if (c == '(')
+                {
+                    parenthesesIndices.Push(result.Length);
+                }
+                else if (c == ')')
+                {
+                    var from = parenthesesIndices.Pop();
+                    Reverse(result, from, result.Length - 1);
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private void Reverse(StringBuilder sb, int from, int to)
+        {
+            while (from < to)
+            {
+                var tmp = sb[from];
+                sb[from++] = sb[to];
+                sb[to--] = tmp;
+            }
+        }
+    }
+}
