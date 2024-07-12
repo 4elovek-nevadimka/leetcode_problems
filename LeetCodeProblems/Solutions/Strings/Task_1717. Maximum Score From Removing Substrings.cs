74137c631728038138e93bfb265899aac090e8e8
Nevadimka <morozovvadimka@gmail.com>
2024-07-12 17:14:54 +0300

    Added LeetCode Problem #1717

---
+++ LeetCodeProblems/Solutions/Strings/Task_1717. Maximum Score From Removing Substrings.cs
@@ -0,0 +1,69 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_1717
+    {
+        // #String #Stack #Greedy
+
+        public int MaximumGain(string s, int x, int y)
+        {
+            var primaryAward = (x < y) ? y : x;
+            var secondaryAward = (x < y) ? x : y;
+            var primaryC = (x < y) ? 'b' : 'a';
+            var secondaryC = (x < y) ? 'a' : 'b';
+            var primaryStack = new Stack<char>();
+            var secondaryStack = new Stack<char>();
+
+            var result = 0;
+
+            foreach (char c in s)
+            {
+                if (c == primaryC)
+                {
+                    primaryStack.Push(c);
+                }
+                else if (c == secondaryC)
+                {
+                    if (primaryStack.Count > 0)
+                    {
+                        primaryStack.Pop();
+                        result += primaryAward;
+                    }
+                    else
+                    {
+                        secondaryStack.Push(c);
+                    }
+                }
+                else
+                {
+                    while (secondaryStack.Count > 0)
+                    {
+                        if (primaryStack.Count > 0)
+                        {
+                            primaryStack.Pop();
+                            secondaryStack.Pop();
+                            result += secondaryAward;
+                        }
+                        else
+                            break;
+                    }
+                    primaryStack.Clear();
+                    secondaryStack.Clear();
+                }
+            }
+
+            while (secondaryStack.Count > 0)
+            {
+                if (primaryStack.Count > 0)
+                {
+                    primaryStack.Pop();
+                    secondaryStack.Pop();
+                    result += secondaryAward;
+                }
+                else
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
