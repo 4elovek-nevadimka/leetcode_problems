9b359c1b3850f07a2b57486853ad598945008b38
Nevadimka <morozovvadimka@gmail.com>
2024-07-19 17:58:47 +0300

    Added LeetCode Problem #1380

---
+++ LeetCodeProblems/Solutions/Matrix/Task_1380. Lucky Numbers in a Matrix.cs
@@ -0,0 +1,40 @@
+﻿namespace Solutions.Matrix
+{
+    internal class Task_1380
+    {
+        // #Array #Matrix
+
+        public void Run()
+        {
+            LuckyNumbers([[3, 7, 8], [9, 11, 13], [15, 16, 17]]);
+            LuckyNumbers([[1, 10, 4, 2], [9, 3, 8, 7], [15, 16, 17, 12]]);
+            LuckyNumbers([[7, 8], [1, 2]]);
+        }
+
+        public IList<int> LuckyNumbers(int[][] matrix)
+        {
+            int m = matrix[0].Length, n = matrix.Length;
+            var minInRows = new int[n];
+            var maxInCols = new int[m];
+
+            for (var i = 0; i < n; i++)
+            {
+                minInRows[i] = int.MaxValue;
+                for (var j = 0; j < m; j++)
+                    minInRows[i] = Math.Min(minInRows[i], matrix[i][j]);
+            }
+
+            for (var i = 0; i < m; i++)
+                for (var j = 0; j < n; j++)
+                    maxInCols[i] = Math.Max(maxInCols[i], matrix[j][i]);
+
+            var result = new List<int>();
+            for (var i = 0; i < n; i++)
+                for (var j = 0; j < m; j++)
+                    if (minInRows[i] == maxInCols[j])
+                        result.Add(minInRows[i]);
+
+            return result;
+        }
+    }
+}
--- LeetCodeProblems/Solutions/Program.cs
+++ LeetCodeProblems/Solutions/Program.cs
@@ -16,6 +16,6 @@ using Solutions.Trie;
 Console.WriteLine("LeetCode Problems!");
 
 // run task here for debugging
-new Task_2196().Run();
+new Task_1380().Run();
 
 // Console.ReadKey();
