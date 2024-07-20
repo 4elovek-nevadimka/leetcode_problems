fe877d5691e19581d3ce2cc14a4562b8bf354dcf
Nevadimka <morozovvadimka@gmail.com>
2024-07-20 21:04:57 +0300

    Added LeetCode Problem #1605

---
+++ LeetCodeProblems/Solutions/Matrix/Task_1605. Find Valid Matrix Given Row and Column Sums.cs
@@ -0,0 +1,25 @@
+﻿namespace Solutions.Matrix
+{
+    internal class Task_1605
+    {
+        // #Array #Greedy #Matrix
+
+        public int[][] RestoreMatrix(int[] rowSum, int[] colSum)
+        {
+            var result = new int[rowSum.Length][];
+            for (int i = 0; i < rowSum.Length; i++)
+                result[i] = new int[colSum.Length];
+
+            for (var i = 0; i < rowSum.Length; i++)
+                for (var j = 0; j < colSum.Length; j++)
+                {
+                    var minVal = Math.Min(rowSum[i], colSum[j]);
+                    result[i][j] = minVal;
+                    rowSum[i] -= minVal;
+                    colSum[j] -= minVal;
+                }
+
+            return result;
+        }
+    }
+}
