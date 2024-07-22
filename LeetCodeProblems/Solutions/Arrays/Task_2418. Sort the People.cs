82e6c8b9887b7ba2dca4a0512f084e4f567ac517
Nevadimka <morozovvadimka@gmail.com>
2024-07-22 16:25:14 +0300

    Added LeetCode Problem #2418

---
+++ LeetCodeProblems/Solutions/Arrays/Task_2418. Sort the People.cs
@@ -0,0 +1,20 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_2418
+    {
+        // #Array #Hash Table #String #Sorting
+
+        public string[] SortPeople(string[] names, int[] heights)
+        {
+            var dic = new Dictionary<int, string>();
+            for (var i = 0; i < names.Length; i++)
+                dic.Add(heights[i], names[i]);
+
+            Array.Sort(heights, (a, b) => a.CompareTo(b));
+            for (var i = 0; i < names.Length; i++)
+                names[i] = dic[heights[i]];
+
+            return names;
+        }
+    }
+}
