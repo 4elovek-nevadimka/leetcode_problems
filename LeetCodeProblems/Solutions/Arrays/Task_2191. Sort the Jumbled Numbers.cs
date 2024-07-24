1138ba322cf6e87058710b8f22b20f56b3d17bff
Nevadimka <morozovvadimka@gmail.com>
2024-07-24 22:51:01 +0300

    One more solution for #2191

--- LeetCodeProblems/Solutions/Arrays/Task_2191. Sort the Jumbled Numbers.cs
+++ LeetCodeProblems/Solutions/Arrays/Task_2191. Sort the Jumbled Numbers.cs
@@ -10,6 +10,7 @@
             var result = SortJumbled([8, 9, 4, 0, 2, 1, 3, 5, 7, 6], [991, 338, 38]);
             Array.ForEach(result, Console.Write);
 
+            Console.WriteLine();
             Console.WriteLine("----------");
             result = SortJumbled([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [789, 456, 123]);
             Array.ForEach(result, Console.Write);
@@ -17,31 +18,38 @@
 
         public int[] SortJumbled(int[] mapping, int[] nums)
         {
-            var mappedNums = new int[nums.Length];
-            for (var i = 0; i < nums.Length; i++)
-                mappedNums[i] = GenerateNewNum(nums[i], mapping);
-
-            Array.Sort(nums, mappedNums);
-            return nums;
-        }
-
-        private int GenerateNewNum(int oldNum, int[] mapping)
-        {
-            var stack = new Stack<int>();
-            while (oldNum > 0)
+            int GetMappedNum(int oldNum)
             {
-                stack.Push(oldNum % 10);
-                oldNum /= 10;
+                if (oldNum == 0)
+                    return mapping[0];
+                var stack = new Stack<int>();
+                while (oldNum > 0)
+                {
+                    stack.Push(oldNum % 10);
+                    oldNum /= 10;
+                }
+
+                var newNum = 0;
+                while (stack.Count > 1)
+                    newNum = (newNum + mapping[stack.Pop()]) * 10;
+
+                newNum += mapping[stack.Pop()];
+
+                // Console.WriteLine(newNum);
+                return newNum;
             }
 
-            var newNum = 0;
-            while (stack.Count > 1)
-                newNum = (newNum + mapping[stack.Pop()]) * 10;
+            var pairs = new List<(int original, int mapped)>();
+            foreach (int num in nums)
+                pairs.Add((num, GetMappedNum(num)));
 
-            newNum += mapping[stack.Pop()];
+            pairs.Sort((a, b) => a.mapped.CompareTo(b.mapped));
 
-            Console.WriteLine(newNum);
-            return newNum;
+            var sortedNums = new int[nums.Length];
+            for (int i = 0; i < pairs.Count; i++)
+                sortedNums[i] = pairs[i].original;
+
+            return sortedNums;
         }
     }
 }
