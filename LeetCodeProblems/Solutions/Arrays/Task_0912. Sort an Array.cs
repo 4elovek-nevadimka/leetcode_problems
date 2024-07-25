8a898138404ade137013f1760f99f5e44b0cab36
Nevadimka <morozovvadimka@gmail.com>
2024-07-25 20:43:54 +0300

    Added LeetCode Problem #0912 - Quick Sort

---
+++ LeetCodeProblems/Solutions/Arrays/Task_0912. Sort an Array.cs
@@ -0,0 +1,48 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_0912
+    {
+        // #Array #Divide and Conquer #Sorting #Heap(Priority Queue) #Merge Sort #Bucket Sort #Radix Sort #Counting Sort
+
+        public int[] SortArray(int[] nums)
+        {
+            QuickSort(nums, 0, nums.Length - 1);
+            return nums;
+        }
+
+        private void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(arr, left, right);
+                QuickSort(arr, left, pivotIndex - 1);
+                QuickSort(arr, pivotIndex + 1, right);
+            }
+        }
+
+        private int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+
+            Swap(arr, i + 1, right);
+            return i + 1;
+        }
+
+        private void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
