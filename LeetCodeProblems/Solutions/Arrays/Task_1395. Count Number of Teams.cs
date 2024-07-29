0773885fa0ca5cda820a95ebf99a7fbc67e23c06
Nevadimka <morozovvadimka@gmail.com>
2024-07-29 21:19:52 +0300

    Added LeetCode Problem #1395

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1395. Count Number of Teams.cs
@@ -0,0 +1,44 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1395
+    {
+        // #Array #Dynamic Programming #Binary Indexed Tree
+
+        public int NumTeams(int[] rating)
+        {
+            var teamsCount = 0;
+            var n = rating.Length;
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = i + 1; j < n; j++)
+                {
+                    if (rating[i] > rating[j])
+                    {
+                        for (var k = j + 1; k < n; k++)
+                        {
+                            if (rating[j] > rating[k])
+                                teamsCount++;
+                        }
+                    }
+                }
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = i + 1; j < n; j++)
+                {
+                    if (rating[i] < rating[j])
+                    {
+                        for (var k = j + 1; k < n; k++)
+                        {
+                            if (rating[j] < rating[k])
+                                teamsCount++;
+                        }
+                    }
+                }
+            }
+
+            return teamsCount;
+        }
+    }
+}
