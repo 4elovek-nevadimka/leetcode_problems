95171253d7bc5542642518c8ada0f5955847a240
Nevadimka <morozovvadimka@gmail.com>
2024-08-01 21:11:05 +0300

    Added LeetCode Problem #2678

---
+++ LeetCodeProblems/Solutions/Strings/Task_2678. Number of Senior Citizens.cs
@@ -0,0 +1,18 @@
+﻿namespace Solutions.Strings
+{
+    internal class Task_2678
+    {
+        // #Array #String
+
+        public int CountSeniors(string[] details)
+        {
+            int count = 0;
+            foreach (var item in details)
+                if ((char.GetNumericValue(item[11]) * 10 
+                    + char.GetNumericValue(item[12])) > 60)
+                    count++;
+
+            return count;
+        }
+    }
+}
