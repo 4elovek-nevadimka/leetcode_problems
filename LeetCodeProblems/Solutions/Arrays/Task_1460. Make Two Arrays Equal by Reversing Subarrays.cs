fbe64b4a1493fcac8c73f25a4e2a6cfa0d87e261
Nevadimka <morozovvadimka@gmail.com>
2024-08-03 18:52:16 +0300

    Added LeetCode Problem #1460

---
+++ LeetCodeProblems/Solutions/Arrays/Task_1460. Make Two Arrays Equal by Reversing Subarrays.cs
@@ -0,0 +1,24 @@
+﻿namespace Solutions.Arrays
+{
+    internal class Task_1460
+    {
+        // #Array #Hash Table #Sorting
+
+        public bool CanBeEqual(int[] target, int[] arr)
+        {
+            var tmpArr = new int[1001];
+            foreach (var item in target)
+                tmpArr[item]++;
+
+            foreach (var item in arr)
+                if (--tmpArr[item] < 0)
+                    return false;
+
+            foreach (var item in tmpArr)
+                if (item > 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
